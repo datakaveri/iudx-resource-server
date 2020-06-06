0a2cfa31af33d6677d95f16cc7e5a4da3a03132a
swaminathanvasanth <vasanth.rajaraman@datakaveri.org>
2020-06-06 16:32:03 +0530

    Added rabbitMQ client setup during Data Broker deployment

--- src/main/java/iudx/resource/server/databroker/DataBrokerVerticle.java
+++ src/main/java/iudx/resource/server/databroker/DataBrokerVerticle.java
@@ -6,11 +6,16 @@ import io.vertx.core.VertxOptions;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.core.spi.cluster.ClusterManager;
+import io.vertx.rabbitmq.RabbitMQClient;
+import io.vertx.rabbitmq.RabbitMQOptions;
 import io.vertx.servicediscovery.Record;
 import io.vertx.servicediscovery.ServiceDiscovery;
 import io.vertx.servicediscovery.types.EventBusService;
 import io.vertx.serviceproxy.ServiceBinder;
 import io.vertx.spi.cluster.hazelcast.HazelcastClusterManager;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
 
 /**
  * The Data Broker Verticle.
@@ -33,6 +38,20 @@ public class DataBrokerVerticle extends AbstractVerticle {
   private ServiceDiscovery discovery;
   private Record record;
   private DataBrokerService databroker;
+  private RabbitMQOptions config;
+  private RabbitMQClient client;
+  private Properties properties;
+  private InputStream inputstream;
+  private String dataBrokerIP;
+  private int dataBrokerPort;
+  private String dataBrokerVhost;
+  private String dataBrokerUserName;
+  private String dataBrokerPassword;
+  private int connectionTimeout;
+  private int requestedHeartbeat;
+  private int handshakeTimeout;
+  private int requestedChannelMax;
+  private int networkRecoveryInterval;
 
   /**
    * This method is used to start the Verticle. It deploys a verticle in a cluster, registers the
@@ -43,25 +62,73 @@ public class DataBrokerVerticle extends AbstractVerticle {
   @Override
   public void start() throws Exception {
 
-    /** Create a reference to HazelcastClusterManager. */
+    /* Create a reference to HazelcastClusterManager. */
 
     mgr = new HazelcastClusterManager();
     options = new VertxOptions().setClusterManager(mgr);
 
-    /** Create or Join a Vert.x Cluster. */
+    /* Create or Join a Vert.x Cluster. */
 
     Vertx.clusteredVertx(options, res -> {
       if (res.succeeded()) {
         vertx = res.result();
 
-        databroker = new DataBrokerServiceImpl();
+        /* Read the configuration and set the rabbitMQ server properties. */
+        properties = new Properties();
+        inputstream = null;
 
-        /** Publish the Data Broker service with the Event Bus against an address. */
+        try {
+
+          inputstream = new FileInputStream("config.properties");
+          properties.load(inputstream);
+
+          dataBrokerIP = properties.getProperty("dataBrokerIP");
+          dataBrokerPort = Integer.parseInt(properties.getProperty("dataBrokerPort"));
+          dataBrokerVhost = properties.getProperty("dataBrokerVhost");
+          dataBrokerUserName = properties.getProperty("dataBrokerUserName");
+          dataBrokerPassword = properties.getProperty("dataBrokerPassword");
+          connectionTimeout = Integer.parseInt(properties.getProperty("connectionTimeout"));
+          requestedHeartbeat = Integer.parseInt(properties.getProperty("requestedHeartbeat"));
+          handshakeTimeout = Integer.parseInt(properties.getProperty("handshakeTimeout"));
+          requestedChannelMax = Integer.parseInt(properties.getProperty("requestedChannelMax"));
+          networkRecoveryInterval =
+              Integer.parseInt(properties.getProperty("networkRecoveryInterval"));
+
+        } catch (Exception ex) {
+
+          logger.info(ex.toString());
+
+        }
+
+        /* Configure the RabbitMQ Data Broker client with input from config files. */
+
+        config = new RabbitMQOptions();
+        config.setUser(dataBrokerUserName);
+        config.setPassword(dataBrokerPassword);
+        config.setHost(dataBrokerIP);
+        config.setPort(dataBrokerPort);
+        config.setVirtualHost(dataBrokerVhost);
+        config.setConnectionTimeout(connectionTimeout);
+        config.setRequestedHeartbeat(requestedHeartbeat);
+        config.setHandshakeTimeout(handshakeTimeout);
+        config.setRequestedChannelMax(requestedChannelMax);
+        config.setNetworkRecoveryInterval(networkRecoveryInterval);
+        config.setAutomaticRecoveryEnabled(true);
+
+        /* Create a RabbitMQ Clinet with the configuration and vertx cluster instance. */
+
+        client = RabbitMQClient.create(vertx, config);
+
+        /* Call the databroker constructor with the RabbitMQ client. */
+
+        databroker = new DataBrokerServiceImpl(client);
+
+        /* Publish the Data Broker service with the Event Bus against an address. */
 
         new ServiceBinder(vertx).setAddress("iudx.rs.databroker.service")
             .register(DataBrokerService.class, databroker);
 
-        /** Get a handler for the Service Discovery interface and publish a service record. */
+        /* Get a handler for the Service Discovery interface and publish a service record. */
 
         discovery = ServiceDiscovery.create(vertx);
         record = EventBusService.createRecord("iudx.rs.databroker.service", // The service name
