bb1bbf5b40b65a7a9caeab4bb6649816ffd44b6b
swaminathanvasanth <vasanth.rajaraman@datakaveri.org>
2020-06-17 12:46:46 +0530

    Updated the RabbitMQ test cases

--- src/test/java/iudx/resource/server/databroker/DataBrokerServiceTest.java
+++ src/test/java/iudx/resource/server/databroker/DataBrokerServiceTest.java
@@ -1,44 +1,151 @@
 package iudx.resource.server.databroker;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import java.net.URLEncoder;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import io.vertx.core.Vertx;
-import io.vertx.core.buffer.Buffer;
 import io.vertx.core.json.JsonObject;
-import io.vertx.ext.web.client.HttpRequest;
-import io.vertx.ext.web.client.HttpResponse;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.web.client.WebClient;
 import io.vertx.ext.web.client.WebClientOptions;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
+import io.vertx.rabbitmq.RabbitMQClient;
+import io.vertx.rabbitmq.RabbitMQOptions;
 
 @ExtendWith(VertxExtension.class)
 public class DataBrokerServiceTest {
 
-  DataBrokerService service;
-  private WebClientOptions webConfig;
-  private WebClient webClient;
+  static DataBrokerService databroker;
+  static private Properties properties;
+  static private InputStream inputstream;
+  static private String dataBrokerIP;
+  static private int dataBrokerPort;
+  static private int dataBrokerManagementPort;
+  static private String dataBrokerVhost;
+  static private String dataBrokerUserName;
+  static private String dataBrokerPassword;
+  static private int connectionTimeout;
+  static private int requestedHeartbeat;
+  static private int handshakeTimeout;
+  static private int requestedChannelMax;
+  static private int networkRecoveryInterval;
+  static private WebClient webClient;
+  static private WebClientOptions webConfig;
+  private static RabbitMQOptions config;
+  private static RabbitMQClient client;
+  private static String exchangeName;
+  private static String queueName;
+  private static JsonObject expected;
 
-  DataBrokerServiceTest() {
-    Vertx vertx = Vertx.vertx();
-    webConfig = new WebClientOptions();
-    webConfig.setKeepAlive(true);
-    webConfig.setConnectTimeout(86400000);
-    webConfig.setDefaultHost("68.183.80.248");
-    webConfig.setDefaultPort(15672);
-    webConfig.setKeepAliveTimeout(86400000);
-
-    webClient = WebClient.create(vertx, webConfig);
-  }
+  private static final Logger logger = LoggerFactory.getLogger(DataBrokerServiceTest.class);
 
   @BeforeAll
   @DisplayName("Deploy a verticle")
   static void startVertx(Vertx vertx, VertxTestContext testContext) {
+
+    /* Choose a Character random from this String */
+    String AlphaNumericString =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "0123456789" + "abcdefghijklmnopqrstuvxyz";
+
+    /* Create StringBuffer size of AlphaNumericString */
+    StringBuilder sb = new StringBuilder(20);
+
+    for (int i = 0; i < 20; i++) {
+
+      /*
+       * Generate a random number between 0 to AlphaNumericString variable length
+       */
+      int index = (int) (AlphaNumericString.length() * Math.random());
+
+      /* Add Character one by one in end of sb */
+      sb.append(AlphaNumericString.charAt(index));
+    }
+
+    exchangeName = sb.toString();
+    queueName = sb.toString();
+
+    logger.info("Exchange Name is " + exchangeName);
+    logger.info("Queue Name is " + queueName);
+
+    /* Read the configuration and set the rabbitMQ server properties. */
+    properties = new Properties();
+    inputstream = null;
+
+    try {
+
+      inputstream = new FileInputStream("config.properties");
+      properties.load(inputstream);
+
+      dataBrokerIP = properties.getProperty("dataBrokerIP");
+      dataBrokerPort = Integer.parseInt(properties.getProperty("dataBrokerPort"));
+      dataBrokerManagementPort =
+          Integer.parseInt(properties.getProperty("dataBrokerManagementPort"));
+      dataBrokerVhost = properties.getProperty("dataBrokerVhost");
+      dataBrokerUserName = properties.getProperty("dataBrokerUserName");
+      dataBrokerPassword = properties.getProperty("dataBrokerPassword");
+      connectionTimeout = Integer.parseInt(properties.getProperty("connectionTimeout"));
+      requestedHeartbeat = Integer.parseInt(properties.getProperty("requestedHeartbeat"));
+      handshakeTimeout = Integer.parseInt(properties.getProperty("handshakeTimeout"));
+      requestedChannelMax = Integer.parseInt(properties.getProperty("requestedChannelMax"));
+      networkRecoveryInterval = Integer.parseInt(properties.getProperty("networkRecoveryInterval"));
+
+    } catch (Exception ex) {
+
+      logger.info(ex.toString());
+
+    }
+
+    /* Configure the RabbitMQ Data Broker client with input from config files. */
+
+    config = new RabbitMQOptions();
+    config.setUser(dataBrokerUserName);
+    config.setPassword(dataBrokerPassword);
+    config.setHost(dataBrokerIP);
+    config.setPort(dataBrokerPort);
+    config.setVirtualHost(dataBrokerVhost);
+    config.setConnectionTimeout(connectionTimeout);
+    config.setRequestedHeartbeat(requestedHeartbeat);
+    config.setHandshakeTimeout(handshakeTimeout);
+    config.setRequestedChannelMax(requestedChannelMax);
+    config.setNetworkRecoveryInterval(networkRecoveryInterval);
+    config.setAutomaticRecoveryEnabled(true);
+
+
+    webConfig = new WebClientOptions();
+    webConfig.setKeepAlive(true);
+    webConfig.setConnectTimeout(86400000);
+    webConfig.setDefaultHost(dataBrokerIP);
+    webConfig.setDefaultPort(dataBrokerManagementPort);
+    webConfig.setKeepAliveTimeout(86400000);
+
+
+    /* Create a RabbitMQ Clinet with the configuration and vertx cluster instance. */
+
+    client = RabbitMQClient.create(vertx, config);
+
+    /* Create a Vertx Web Client with the configuration and vertx cluster instance. */
+
+    webClient = WebClient.create(vertx, webConfig);
+
+    /* Create a Json Object for properties */
+
+    JsonObject propObj = new JsonObject();
+
+    propObj.put("userName", dataBrokerUserName);
+    propObj.put("password", dataBrokerPassword);
+    propObj.put("vHost", dataBrokerVhost);
+
+    /* Call the databroker constructor with the RabbitMQ client. */
+
+    databroker = new DataBrokerServiceImpl(client, webClient, propObj);
 
     vertx.deployVerticle(new DataBrokerVerticle(),
         testContext.succeeding(id -> testContext.completeNow()));
@@ -46,51 +153,45 @@ public class DataBrokerServiceTest {
 
 
   @Test
-  @Disabled
+  @DisplayName("Testing Create Exchange")
+  @Order(1)
   void successCreateExchange(VertxTestContext testContext) {
+    JsonObject expected = new JsonObject();
+    expected.put("Exchange", exchangeName);
+
     JsonObject request = new JsonObject();
-    System.out.println("INSIDE successTestEvent");
-    request.put("exchangeName", "ankita_exchange11");
-    String exchangeName = request.getString("exchangeName");
-    String vhost = URLEncoder.encode("/");
-    String url = "/api/exchanges/" + vhost + "/" + exchangeName;
-    JsonObject obj = new JsonObject();
-    obj.put("type", Constants.EXCHANGE_TYPE);
-    obj.put("auto_delete", false);
-    obj.put("durable", true);
-    HttpRequest<Buffer> webRequest =
-        webClient.put(url).basicAuthentication("IUDXDemoUser", "IUDXDemoUser@123");
-    webRequest.sendJsonObject(obj, testContext.succeeding(response -> testContext.verify(() -> {
-      System.out.println(response.statusCode());
-      assertEquals(response.statusCode(), 201);
+    request.put("exchangeName", exchangeName);
+
+    databroker.createExchange(request, handler -> {
+      if (handler.succeeded()) {
+        JsonObject response = handler.result();
+        logger.info("Create Exchange response is : " + response);
+        assertEquals(expected, response);
+      }
       testContext.completeNow();
-    })));
+    });
   }
 
   @Test
+  @DisplayName("Testing Create Queue")
+  @Order(2)
   void successCreateQueue(VertxTestContext testContext) {
-    JsonObject request = new JsonObject();
-    System.out.println("INSIDE successTestEvent");
-    request.put("queueName", "ankita_queue11");
-    String queueName = request.getString("queueName");
-    String vhost = URLEncoder.encode("/");
-    String url = "/api/exchanges/" + vhost + "/" + queueName;
-    String user = "IUDXDemoUser";
-    String password = "IUDXDemoUser@123";
-    url = "/api/queues/" + vhost + "/" + queueName;
-    JsonObject configProp = new JsonObject();
-    configProp.put("x-message-ttl", Constants.X_MESSAGE_TTL);
-    configProp.put("x-max-length", Constants.X_MAXLENGTH);
-    configProp.put("x-queue-mode", Constants.X_QUEQUE_MODE);
-    HttpRequest<Buffer> webRequest = webClient.put(url).basicAuthentication(user, password);
-    webRequest.sendJsonObject(configProp,
-        testContext.succeeding(response -> testContext.verify(() -> {
-          System.out.println(response.statusCode());
-          assertEquals(response.statusCode(), 201);
-          testContext.completeNow();
-        })));
-  }
+    JsonObject expected = new JsonObject();
+    expected.put("queue", queueName);
 
+    JsonObject request = new JsonObject();
+    request.put("queueName", queueName);
+
+    databroker.createQueue(request, handler -> {
+      if (handler.succeeded()) {
+        JsonObject response = handler.result();
+        logger.info("Create Queue response is : " + response);
+        assertEquals(expected, response);
+      }
+      testContext.completeNow();
+    });
+
+  }
 }
 
 
