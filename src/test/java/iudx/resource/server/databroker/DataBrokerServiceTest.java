723555b7b3d225b2aa32fc4800c224a95a58e703
Ankita Mishra <ankita.mishra@trigyn.com>
2020-06-18 13:36:24 +0530

    added test cases for all the service methods

--- src/test/java/iudx/resource/server/databroker/DataBrokerServiceTest.java
+++ src/test/java/iudx/resource/server/databroker/DataBrokerServiceTest.java
@@ -6,10 +6,13 @@ import java.io.InputStream;
 import java.util.Properties;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
 import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
@@ -21,6 +24,7 @@ import io.vertx.rabbitmq.RabbitMQClient;
 import io.vertx.rabbitmq.RabbitMQOptions;
 
 @ExtendWith(VertxExtension.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class DataBrokerServiceTest {
 
   static DataBrokerService databroker;
@@ -43,7 +47,13 @@ public class DataBrokerServiceTest {
   private static RabbitMQClient client;
   private static String exchangeName;
   private static String queueName;
+  private static JsonArray entities;
   private static JsonObject expected;
+  static JsonObject propObj;
+  private static String vHost;
+  private static int statusOk;
+  private static int statusNotFound;
+  private static int statusNoContent;
 
   private static final Logger logger = LoggerFactory.getLogger(DataBrokerServiceTest.class);
 
@@ -71,6 +81,12 @@ public class DataBrokerServiceTest {
 
     exchangeName = sb.toString();
     queueName = sb.toString();
+    entities = new JsonArray("[\"id1\", \"id2\"]");
+    vHost = "IUDX";
+    statusOk = 200;
+    statusNotFound = 404;
+    statusNoContent = 204;
+
 
     logger.info("Exchange Name is " + exchangeName);
     logger.info("Queue Name is " + queueName);
@@ -137,7 +153,7 @@ public class DataBrokerServiceTest {
 
     /* Create a Json Object for properties */
 
-    JsonObject propObj = new JsonObject();
+    propObj = new JsonObject();
 
     propObj.put("userName", dataBrokerUserName);
     propObj.put("password", dataBrokerPassword);
@@ -156,8 +172,32 @@ public class DataBrokerServiceTest {
   @DisplayName("Testing Create Exchange")
   @Order(1)
   void successCreateExchange(VertxTestContext testContext) {
+
     JsonObject expected = new JsonObject();
-    expected.put("Exchange", exchangeName);
+    expected.put("exchange", exchangeName);
+
+    JsonObject request = new JsonObject();
+    request.put("exchangeName", exchangeName);
+
+    databroker.createExchange(request, handler -> {
+      if (handler.succeeded()) {
+        JsonObject response = handler.result();
+        logger.info("Create Exchange response is : " + response);
+        assertEquals(expected, response);
+      }
+      testContext.completeNow();
+    });
+  }
+
+  @Test
+  @DisplayName("Creating already existing exchange")
+  @Order(2)
+  void failCreateExchange(VertxTestContext testContext) {
+
+    JsonObject expected = new JsonObject();
+    expected.put("type", statusNoContent);
+    expected.put("title", "Failure");
+    expected.put("detail", "Exchange already exists");
 
     JsonObject request = new JsonObject();
     request.put("exchangeName", exchangeName);
@@ -174,8 +214,9 @@ public class DataBrokerServiceTest {
 
   @Test
   @DisplayName("Testing Create Queue")
-  @Order(2)
+  @Order(3)
   void successCreateQueue(VertxTestContext testContext) {
+
     JsonObject expected = new JsonObject();
     expected.put("queue", queueName);
 
@@ -192,6 +233,326 @@ public class DataBrokerServiceTest {
     });
 
   }
+
+  @Test
+  @DisplayName("Creating already existing queue")
+  @Order(4)
+  void failCreateQueue(VertxTestContext testContext) {
+
+    JsonObject expected = new JsonObject();
+    expected.put("type", statusNoContent);
+    expected.put("title", "Failure");
+    expected.put("detail", "Queue already exists");
+
+    JsonObject request = new JsonObject();
+    request.put("queueName", queueName);
+
+    databroker.createQueue(request, handler -> {
+      if (handler.succeeded()) {
+        JsonObject response = handler.result();
+        logger.info("Create Exchange response is : " + response);
+        assertEquals(expected, response);
+      }
+      testContext.completeNow();
+    });
+  }
+
+  @Test
+  @DisplayName("Binding Exchange and Queue")
+  @Order(5)
+  void successBindQueue(VertxTestContext testContext) {
+
+    JsonObject expected = new JsonObject();
+    expected.put("queue", queueName);
+    expected.put("exchange", exchangeName);
+    expected.put("entities", entities);
+
+    JsonObject request = new JsonObject();
+    request.put("queueName", queueName);
+    request.put("exchangeName", exchangeName);
+    request.put("entities", entities);
+
+    databroker.bindQueue(request, handler -> {
+      if (handler.succeeded()) {
+        JsonObject response = handler.result();
+        logger.info("Bind Queue response is : " + response);
+        assertEquals(expected, response);
+      }
+      testContext.completeNow();
+    });
+  }
+
+  @Test
+  @DisplayName("Listing all bindings of exchange")
+  @Order(6)
+  void successListExchangeBindings(VertxTestContext testContext) {
+
+    JsonObject expected = new JsonObject();
+    expected.put(queueName, entities);
+
+    JsonObject request = new JsonObject();
+    request.put("exchangeName", exchangeName);
+
+    databroker.listExchangeSubscribers(request, handler -> {
+      if (handler.succeeded()) {
+        JsonObject response = handler.result();
+        logger.info("List exchnage bindings response is : " + response);
+        assertEquals(expected, response);
+      }
+      testContext.completeNow();
+    });
+
+  }
+
+
+
+  @Test
+  @DisplayName("Listing all bindings of queue")
+  @Order(7)
+  void successListQueueBindings(VertxTestContext testContext) {
+
+    JsonObject expected = new JsonObject();
+    expected.put("entities", entities);
+
+    JsonObject request = new JsonObject();
+    request.put("queueName", queueName);
+
+    databroker.listQueueSubscribers(request, handler -> {
+      if (handler.succeeded()) {
+        JsonObject response = handler.result();
+        logger.info("List queue bindings response is : " + response);
+        assertEquals(expected, response);
+      }
+      testContext.completeNow();
+    });
+
+  }
+
+  @Test
+  @DisplayName("Unbinding Exchange and Queue")
+  @Order(8)
+  void successUnbindQueue(VertxTestContext testContext) {
+
+    JsonObject expected = new JsonObject();
+    expected.put("queue", queueName);
+    expected.put("exchange", exchangeName);
+    expected.put("entities", entities);
+
+    JsonObject request = new JsonObject();
+    request.put("queueName", queueName);
+    request.put("exchangeName", exchangeName);
+    request.put("entities", entities);
+
+    databroker.unbindQueue(request, handler -> {
+      if (handler.succeeded()) {
+        JsonObject response = handler.result();
+        logger.info("Unbind Queue response is : " + response);
+        assertEquals(expected, response);
+      }
+      testContext.completeNow();
+    });
+
+  }
+
+  @Test
+  @DisplayName("Deleting a Queue")
+  @Order(9)
+  void successDeleteQueue(VertxTestContext testContext) {
+
+    JsonObject expected = new JsonObject();
+    expected.put("queue", queueName);
+
+    JsonObject request = new JsonObject();
+    request.put("queueName", queueName);
+
+    databroker.deleteQueue(request, handler -> {
+      if (handler.succeeded()) {
+        JsonObject response = handler.result();
+        logger.info("Delete Queue response is : " + response);
+        assertEquals(expected, response);
+      }
+      testContext.completeNow();
+    });
+
+  }
+
+  @Test
+  @DisplayName("Deleting an already deleted Queue")
+  @Order(10)
+  void failDeleteQueue(VertxTestContext testContext) {
+
+    JsonObject expected = new JsonObject();
+    expected.put("type", statusNotFound);
+    expected.put("title", "Failure");
+    expected.put("detail", "Queue does not exist");
+
+    JsonObject request = new JsonObject();
+    request.put("queueName", queueName);
+
+    databroker.deleteQueue(request, handler -> {
+      if (handler.succeeded()) {
+        JsonObject response = handler.result();
+        logger.info("Delete Queue response is : " + response);
+        assertEquals(expected, response);
+      }
+      testContext.completeNow();
+    });
+
+  }
+
+  @Test
+  @DisplayName("Deleting an Exchange")
+  @Order(11)
+  void successDeleteExchange(VertxTestContext testContext) {
+
+    JsonObject expected = new JsonObject();
+    expected.put("exchange", exchangeName);
+
+    JsonObject request = new JsonObject();
+    request.put("exchangeName", exchangeName);
+
+    databroker.deleteExchange(request, handler -> {
+      if (handler.succeeded()) {
+        JsonObject response = handler.result();
+        logger.info("Delete Exchange response is : " + response);
+        assertEquals(expected, response);
+      }
+      testContext.completeNow();
+    });
+
+  }
+
+  @Test
+  @DisplayName("Deleting an already deleted Exchange")
+  @Order(12)
+  void failDeleteExchange(VertxTestContext testContext) {
+
+    JsonObject expected = new JsonObject();
+    expected.put("type", statusNotFound);
+    expected.put("title", "Failure");
+    expected.put("detail", "Exchange does not exist");
+
+    JsonObject request = new JsonObject();
+    request.put("exchangeName", exchangeName);
+
+    databroker.deleteExchange(request, handler -> {
+      if (handler.succeeded()) {
+        JsonObject response = handler.result();
+        logger.info("Delete Exchange response is : " + response);
+        assertEquals(expected, response);
+      }
+      testContext.completeNow();
+    });
+
+  }
+
+  @Test
+  @DisplayName("Creating a Virtual Host")
+  @Order(13)
+  void successCreateVhost(VertxTestContext testContext) {
+
+    JsonObject expected = new JsonObject();
+    expected.put("vHost", vHost);
+
+    JsonObject request = new JsonObject();
+    request.put("vHost", vHost);
+
+    databroker.createvHost(request, handler -> {
+      if (handler.succeeded()) {
+        JsonObject response = handler.result();
+        logger.info("Create vHost response is : " + response);
+        assertEquals(expected, response);
+      }
+      testContext.completeNow();
+    });
+
+  }
+
+  @Test
+  @DisplayName("Deleting a Virtual Host")
+  @Order(14)
+  void successDeleteVhost(VertxTestContext testContext) {
+
+    JsonObject expected = new JsonObject();
+    expected.put("vHost", vHost);
+
+    JsonObject request = new JsonObject();
+    request.put("vHost", vHost);
+
+    databroker.deletevHost(request, handler -> {
+      if (handler.succeeded()) {
+        JsonObject response = handler.result();
+        logger.info("Delete vHost response is : " + response);
+        assertEquals(expected, response);
+      }
+      testContext.completeNow();
+    });
+
+  }
+
+  @Test
+  @DisplayName("List all Virtual Hosts")
+  @Order(15)
+  void successListVhosts(VertxTestContext testContext) {
+
+    JsonObject expected = new JsonObject();
+    expected.put("vHost", new JsonArray("[\"/\",\"test\"]"));
+
+    JsonObject request = new JsonObject();
+
+    databroker.listvHost(request, handler -> {
+      if (handler.succeeded()) {
+        JsonObject response = handler.result();
+        logger.info("List vHost response is : " + response);
+        assertEquals(expected, response);
+      }
+      testContext.completeNow();
+    });
+
+  }
+
+  @Test
+  @DisplayName("Publish message to exchange")
+  @Order(16)
+  void successPublishMessage(VertxTestContext testContext) {
+
+    JsonObject expected = new JsonObject();
+    expected.put("status", statusOk);
+
+    JsonObject request = new JsonObject();
+    request.put("PM2_5", 4.14);
+    request.put("AQI", 64);
+    request.put("CO2", 529.23);
+    request.put("CO", 0.75);
+    request.put("SoundMax", 100.82);
+    request.put("PM10", 4.83);
+    request.put("Rainfall", 0);
+    request.put("id", "ankita_exchange/EM_01_0103_01");
+    request.put("SoundMin", 73.54);
+    request.put("Avg_Humidity", 100);
+    request.put("SO2", 17.95);
+    request.put("AmbientLight", 32040.91);
+    request.put("Pressure", 994.02);
+    request.put("ULTRA_VIOLET", 1.67);
+    request.put("Avg_Temp", 24.9);
+    request.put("Date", "2020-06-05T09:00:01+05:30");
+    request.put("O3", 34.59);
+    request.put("O2", 19.66);
+    request.put("NO2", 50.62);
+
+
+    databroker.publishFromAdaptor(request, handler -> {
+      if (handler.succeeded()) {
+        JsonObject response = handler.result();
+        logger.info("Message from adaptor response is : " + response);
+        assertEquals(expected, response);
+      }
+      testContext.completeNow();
+    });
+
+  }
+
+
 }
 
 
