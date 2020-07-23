069c0eb66f1c9060dee2353260b90fde3f2ede59
Mahidhar <mahidhar.chellamani@datakaveri.org>
2020-07-23 16:27:36 +0530

    Add Constants.java file for the authenticator

---
+++ src/main/java/iudx/resource/server/authenticator/Constants.java
@@ -0,0 +1,9 @@
+package iudx.resource.server.authenticator;
+
+public class Constants {
+    public static final String CONFIG_FILE = "config.properties";
+    public static final String KEYSTORE_PATH = "keystore";
+    public static final String KEYSTORE_PASSWORD = "keystorePassword";
+    public static final String AUTH_SERVER_HOST = "authServerHost";
+    public static final String AUTH_CERTINFO_PATH = "/auth/v1/certificate-info";
+}
