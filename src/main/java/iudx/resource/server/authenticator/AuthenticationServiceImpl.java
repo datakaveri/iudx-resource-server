6fb1e677e001ebbe96be98d6ab88362ad499f2fa
Mahidhar <mahidhar.chellamani@datakaveri.org>
2020-07-29 12:13:49 +0530

    Add skeleton code for the TIP request

--- src/main/java/iudx/resource/server/authenticator/AuthenticationServiceImpl.java
+++ src/main/java/iudx/resource/server/authenticator/AuthenticationServiceImpl.java
@@ -1,16 +1,21 @@
 package iudx.resource.server.authenticator;
 
-import io.vertx.core.AsyncResult;
-import io.vertx.core.Future;
-import io.vertx.core.Handler;
+import io.vertx.core.*;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
+import io.vertx.ext.web.client.HttpResponse;
 import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.client.predicate.ResponsePredicate;
+import org.apache.http.HttpStatus;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Properties;
+import java.util.stream.Collectors;
 
 /**
  * The Authentication Service Implementation.
@@ -48,9 +53,56 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     @Override
     public AuthenticationService tokenInterospect(JsonObject request, JsonObject authenticationInfo,
                                                   Handler<AsyncResult<JsonObject>> handler) {
-        // added for testing.
-        JsonObject json = new JsonObject();
-        handler.handle(Future.succeededFuture(json));
+
+        String token = authenticationInfo.getString("token", Constants.PUBLIC_TOKEN);
+        String requestEndpoint = authenticationInfo.getString("apiEndpoint");
+        if (token.equals(Constants.PUBLIC_TOKEN) && !Constants.OPEN_ENDPOINTS.contains(requestEndpoint)) {
+            JsonObject result = new JsonObject();
+            result.put("status", "error");
+            result.put("message", "Public token cannot access requested endpoint");
+            handler.handle(Future.succeededFuture(result));
+            return this;
+        }
+
+        Future<JsonObject> tipResponseFut = retrieveTipResponse(token);
+        Future<HashMap<String, Boolean>> catResponseFut = isOpenResource(request.getJsonArray("ids"));
+
+        CompositeFuture.all(tipResponseFut, catResponseFut)
+                .onFailure(throwable -> {
+                    JsonObject result = new JsonObject();
+                    result.put("status", "error");
+                    result.put("message", throwable.getMessage());
+                    handler.handle(Future.succeededFuture(result));
+                })
+                .onSuccess(compositeFuture -> {
+                    JsonObject tipResponse = compositeFuture.resultAt(0);
+                    HashMap<String, Boolean> catResponse = compositeFuture.resultAt(1);
+                    JsonArray result = new JsonArray();
+
+                    for (Object reqID : request.getJsonArray("ids")) {
+                        String requestID = (String) reqID;
+                        JsonObject tipRequest = retrieveTipRequest(requestID, tipResponse);
+                        boolean isAccessible = tipRequest.isEmpty() ?
+                                catResponse.getOrDefault(requestID, false) :
+                                isValidEndpoint(requestEndpoint, tipRequest.getJsonArray("apis"));
+                        result.add(new JsonObject()
+                                .put("id", requestID)
+                                .put("accessible", isAccessible));
+                    }
+
+                    JsonArray inAccessibleIDs = new JsonArray(result.stream()
+                            .filter(res -> !(((JsonObject) res).getBoolean("accessible")))
+                            .collect(Collectors.toList()));
+
+                    JsonObject json = new JsonObject();
+                    json.put("status", inAccessibleIDs.isEmpty() ? "success" : "error");
+                    json.put("body", new JsonObject());
+                    if (!inAccessibleIDs.isEmpty()) {
+                        json.put("message", "Unauthorized resource IDs");
+                        json.getJsonObject("body").put("rejected", inAccessibleIDs);
+                    }
+                    handler.handle(Future.succeededFuture(json));
+                });
         return this;
     }
 
--- src/main/java/iudx/resource/server/authenticator/Constants.java
+++ src/main/java/iudx/resource/server/authenticator/Constants.java
@@ -1,9 +1,14 @@
 package iudx.resource.server.authenticator;
 
+import java.util.List;
+
 public class Constants {
     public static final String CONFIG_FILE = "config.properties";
     public static final String KEYSTORE_PATH = "keystore";
     public static final String KEYSTORE_PASSWORD = "keystorePassword";
     public static final String AUTH_SERVER_HOST = "authServerHost";
     public static final String AUTH_CERTINFO_PATH = "/auth/v1/certificate-info";
+    public static final String PUBLIC_TOKEN = "public";
+    public static final List<String> OPEN_ENDPOINTS = List.of("/ngsi-ld/v1/temporal/entities", "/ngsi-ld/v1/entities");
+    public static final String AUTH_TIP_PATH = "/auth/v1/token/introspect";
 }
