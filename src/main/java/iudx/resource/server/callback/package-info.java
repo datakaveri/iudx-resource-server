02d3d9cb8aed272fc67588421c8c8fe79b034808
Rajeev Kushwaha <rajeev.kushwaha1@nectechnologies.in>
2020-08-14 17:05:30 +0530

    Code Commiting for Callback subscription (Connect to RabbitMQ queue, Query Database, Send data to callbackurl)

--- src/main/java/iudx/resource/server/callback/package-info.java
+++ src/main/java/iudx/resource/server/callback/package-info.java
@@ -1,1 +1,6 @@
+
+@ModuleGen(groupPackage = "iudx.resource.server.callback",
+name = "iudx-resource-server-callback-service")
 package iudx.resource.server.callback;
+import io.vertx.codegen.annotations.ModuleGen;
+
