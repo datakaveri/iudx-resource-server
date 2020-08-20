d2b6ef45257f3fca433047b6833220c4f666bb35
Rajeev Kushwaha <rajeev.kushwaha1@nectechnologies.in>
2020-08-20 17:44:11 +0530

    Review Comments Incorporate and add google-checkstyle

--- src/main/java/iudx/resource/server/callback/CallbackService.java
+++ src/main/java/iudx/resource/server/callback/CallbackService.java
@@ -3,21 +3,19 @@ package iudx.resource.server.callback;
 import io.vertx.codegen.annotations.Fluent;
 import io.vertx.codegen.annotations.ProxyGen;
 import io.vertx.codegen.annotations.VertxGen;
-import io.vertx.core.AsyncResult;
-import io.vertx.core.Handler;
+import io.vertx.core.Future;
 import io.vertx.core.json.JsonObject;
 
 /**
- * <h1>Callback Service</h1>
- * The Callback Service.
+ * <h1>Callback Service</h1> The Callback Service.
  * <p>
- * The Callback Service in the IUDX Resource Server defines the operations to be
- * performed with the IUDX Callback server.
+ * The Callback Service in the IUDX Resource Server defines the operations to be performed with the
+ * IUDX Callback server.
  * </p>
  * 
  * @see io.vertx.codegen.annotations.ProxyGen
  * @see io.vertx.codegen.annotations.VertxGen
- * @version 1.0 
+ * @version 1.0
  * @since 2020-05-31
  */
 
@@ -25,48 +23,40 @@ import io.vertx.core.json.JsonObject;
 @ProxyGen
 public interface CallbackService {
 
-	/**
-	 * The getQueueMessage implements for getting message from "callback.data" queue.
-	 * 
-	 * @param request which is a JsonObject
-	 * @param handler which is a Request Handler
-	 * @return CallbackService which is a Service
-	 */
-	@Fluent
-	CallbackService getMessageFromCallbackNotificationQueue(JsonObject request, Handler<AsyncResult<JsonObject>> handler);
-	
-	/**
-	 * The getQueueMessage implements for getting message from "callback.notification" queue.
-	 * 
-	 * @param request which is a JsonObject
-	 * @param handler which is a Request Handler
-	 * @return CallbackService which is a Service
-	 */
-	@Fluent
-	CallbackService getMessageFromCallbackDataQueue(JsonObject request, Handler<AsyncResult<JsonObject>> handler);
-	
-	/**
-	 * The queryCallBackDataBase implements for the query callBack database.
-	 * 
-	 * @param request which is a JsonObject
-	 * @param handler which is a Request Handler
-	 * @return CallbackService which is a Service
-	 */
-	@Fluent
-	CallbackService queryCallBackDataBase(JsonObject request, Handler<AsyncResult<JsonObject>> handler);
+  /**
+   * The connectToCallbackNotificationQueue implements for getting message from
+   * "callback.notification" queue.
+   * 
+   * @param json containing queueName.
+   * @return Future object
+   */
+  @Fluent
+  Future<JsonObject> connectToCallbackNotificationQueue(JsonObject request);
 
-	/**
-	 * <h1>The sendDataToCallBackSubscriber implements for sending data to callback database
-	 * update info.</h1>
-	 * 
-	 * @param request JSON Object
-	 * @param handler Handler to return response in case of success and appropriate
-	 *                error message in case of failure.
-	 */
-	@Fluent
-	CallbackService sendDataToCallBackSubscriber(JsonObject request, Handler<AsyncResult<JsonObject>> handler);
+  /**
+   * The connectToCallbackDataQueue implements for getting message from "callback.data" queue.
+   * 
+   * @param json containing queueName.
+   * @return Future object
+   */
+  @Fluent
+  Future<JsonObject> connectToCallbackDataQueue(JsonObject request);
 
-	
-	
-	
+  /**
+   * The queryCallBackDataBase implements for the query callBack database.
+   * 
+   * @param json containing queueName.
+   * @return Future object
+   */
+  @Fluent
+  Future<JsonObject> queryCallBackDataBase(JsonObject request);
+
+  /**
+   * The sendDataToCallBackSubscriber implements for sending data to callback database update info.
+   * 
+   * @param json containing queueName.
+   * @return Future object
+   */
+  @Fluent
+  Future<JsonObject> sendDataToCallBackSubscriber(JsonObject request);
 }
