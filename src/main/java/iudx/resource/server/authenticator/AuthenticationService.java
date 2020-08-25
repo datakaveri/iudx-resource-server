65e0ab225760719a399c15fabe3d30199d07a5d3
swaminathanvasanth <vasanth.rajaraman@datakaveri.org>
2020-08-25 20:19:17 +0530

    Refactored to checkstyle suggestions

--- src/main/java/iudx/resource/server/authenticator/AuthenticationService.java
+++ src/main/java/iudx/resource/server/authenticator/AuthenticationService.java
@@ -13,8 +13,8 @@ import io.vertx.core.json.JsonObject;
  * The Authentication Service.
  * <h1>Authentication Service</h1>
  * <p>
- * The Authentication Service in the IUDX Resource Server defines the operations
- * to be performed with the IUDX Authentication and Authorization server.
+ * The Authentication Service in the IUDX Resource Server defines the operations to be performed
+ * with the IUDX Authentication and Authorization server.
  * </p>
  *
  * @version 1.0
@@ -27,31 +27,32 @@ import io.vertx.core.json.JsonObject;
 @ProxyGen
 public interface AuthenticationService {
 
-    /**
-     * The createProxy helps the code generation blocks to generate proxy code.
-     *
-     * @param vertx   which is the vertx instance
-     * @param address which is the proxy address
-     * @return AuthenticationServiceVertxEBProxy which is a service proxy
-     */
+  /**
+   * The createProxy helps the code generation blocks to generate proxy code.
+   *
+   * @param vertx which is the vertx instance
+   * @param address which is the proxy address
+   * @return AuthenticationServiceVertxEBProxy which is a service proxy
+   */
 
-    @GenIgnore
-    static AuthenticationService createProxy(Vertx vertx, String address) {
-        return new AuthenticationServiceVertxEBProxy(vertx, address);
-    }
+  @GenIgnore
+  static AuthenticationService createProxy(Vertx vertx, String address) {
+    return new AuthenticationServiceVertxEBProxy(vertx, address);
+  }
 
-    /**
-     * The tokenInterospect method implements the authentication and authorization
-     * module using IUDX APIs. It caches the result of the TIP from the auth server for a duration specified by the
-     * Constants TIP_CACHE_TIMEOUT_AMOUNT and TIP_CACHE_TIMEOUT_UNIT.
-     *
-     * @param request            which is a JsonObject containing ids: [String]
-     * @param authenticationInfo which is a JsonObject containing token: String and apiEndpoint: String
-     * @param handler            which is a request handler
-     * @return AuthenticationService which is a service
-     */
+  /**
+   * The tokenInterospect method implements the authentication and authorization module using IUDX
+   * APIs. It caches the result of the TIP from the auth server for a duration specified by the
+   * Constants TIP_CACHE_TIMEOUT_AMOUNT and TIP_CACHE_TIMEOUT_UNIT.
+   *
+   * @param request which is a JsonObject containing ids: [String]
+   * @param authenticationInfo which is a JsonObject containing token: String and apiEndpoint:
+   *        String
+   * @param handler which is a request handler
+   * @return AuthenticationService which is a service
+   */
 
-    @Fluent
-    AuthenticationService tokenInterospect(JsonObject request, JsonObject authenticationInfo,
-                                           Handler<AsyncResult<JsonObject>> handler);
+  @Fluent
+  AuthenticationService tokenInterospect(JsonObject request, JsonObject authenticationInfo,
+      Handler<AsyncResult<JsonObject>> handler);
 }
--- src/main/java/iudx/resource/server/authenticator/AuthenticationVerticle.java
+++ src/main/java/iudx/resource/server/authenticator/AuthenticationVerticle.java
@@ -14,7 +14,6 @@ import io.vertx.servicediscovery.ServiceDiscovery;
 import io.vertx.servicediscovery.types.EventBusService;
 import io.vertx.serviceproxy.ServiceBinder;
 import io.vertx.spi.cluster.hazelcast.HazelcastClusterManager;
-
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
@@ -33,85 +32,87 @@ import java.util.Properties;
 
 public class AuthenticationVerticle extends AbstractVerticle {
 
-    private static final Logger logger = LoggerFactory.getLogger(AuthenticationVerticle.class);
-    private final Properties properties = new Properties();
-    private Vertx vertx;
-    private ClusterManager mgr;
-    private VertxOptions options;
-    private ServiceDiscovery discovery;
-    private Record record;
-    private AuthenticationService authentication;
+  private static final Logger logger = LoggerFactory.getLogger(AuthenticationVerticle.class);
+  private final Properties properties = new Properties();
+  private Vertx vertx;
+  private ClusterManager mgr;
+  private VertxOptions options;
+  private ServiceDiscovery discovery;
+  private Record record;
+  private AuthenticationService authentication;
 
-    static WebClient createWebClient(Vertx vertx, Properties properties) {
-        return createWebClient(vertx, properties, false);
+  static WebClient createWebClient(Vertx vertx, Properties properties) {
+    return createWebClient(vertx, properties, false);
+  }
+
+  static WebClient createWebClient(Vertx vertxObj, Properties properties, boolean testing) {
+    try {
+      FileInputStream configFile = new FileInputStream(Constants.CONFIG_FILE);
+      if (properties.isEmpty()) {
+        properties.load(configFile);
+      }
+    } catch (IOException e) {
+      logger.error("Could not load properties from config file", e);
     }
-
-    static WebClient createWebClient(Vertx vertxObj, Properties properties, boolean testing) {
-        try {
-            FileInputStream configFile = new FileInputStream(Constants.CONFIG_FILE);
-            if (properties.isEmpty()) properties.load(configFile);
-        } catch (IOException e) {
-            logger.error("Could not load properties from config file", e);
-        }
-        WebClientOptions webClientOptions = new WebClientOptions();
-        if (testing) webClientOptions.setTrustAll(true).setVerifyHost(false);
-        webClientOptions
-                .setSsl(true)
-                .setKeyStoreOptions(new JksOptions()
-                        .setPath(properties.getProperty(Constants.KEYSTORE_PATH))
-                        .setPassword(properties.getProperty(Constants.KEYSTORE_PASSWORD)));
-        return WebClient.create(vertxObj, webClientOptions);
+    WebClientOptions webClientOptions = new WebClientOptions();
+    if (testing) {
+      webClientOptions.setTrustAll(true).setVerifyHost(false);
     }
+    webClientOptions.setSsl(true).setKeyStoreOptions(
+        new JksOptions().setPath(properties.getProperty(Constants.KEYSTORE_PATH))
+            .setPassword(properties.getProperty(Constants.KEYSTORE_PASSWORD)));
+    return WebClient.create(vertxObj, webClientOptions);
+  }
 
-    /**
-     * This method is used to start the Verticle. It deploys a verticle in a cluster, registers the
-     * service with the Event bus against an address, publishes the service with the service discovery
-     * interface.
-     *
-     * @throws Exception which is a startup exception
-     */
+  /**
+   * This method is used to start the Verticle. It deploys a verticle in a cluster, registers the
+   * service with the Event bus against an address, publishes the service with the service discovery
+   * interface.
+   *
+   * @throws Exception which is a startup exception
+   */
 
-    @Override
-    public void start() throws Exception {
+  @Override
+  public void start() throws Exception {
 
-        /* Create a reference to HazelcastClusterManager. */
+    /* Create a reference to HazelcastClusterManager. */
 
-        mgr = new HazelcastClusterManager();
-        options = new VertxOptions().setClusterManager(mgr);
+    mgr = new HazelcastClusterManager();
+    options = new VertxOptions().setClusterManager(mgr);
 
-        /* Create or Join a Vert.x Cluster. */
+    /* Create or Join a Vert.x Cluster. */
 
-        Vertx.clusteredVertx(options, res -> {
-            if (res.succeeded()) {
-                vertx = res.result();
+    Vertx.clusteredVertx(options, res -> {
+      if (res.succeeded()) {
+        vertx = res.result();
 
-                authentication = new AuthenticationServiceImpl(vertx, createWebClient(vertx, properties));
+        authentication = new AuthenticationServiceImpl(vertx, createWebClient(vertx, properties));
 
-                /* Publish the Authentication service with the Event Bus against an address. */
+        /* Publish the Authentication service with the Event Bus against an address. */
 
-                new ServiceBinder(vertx).setAddress("iudx.rs.authentication.service")
-                        .register(AuthenticationService.class, authentication);
+        new ServiceBinder(vertx).setAddress("iudx.rs.authentication.service")
+            .register(AuthenticationService.class, authentication);
 
-                /* Get a handler for the Service Discovery interface and publish a service record. */
+        /* Get a handler for the Service Discovery interface and publish a service record. */
 
-                discovery = ServiceDiscovery.create(vertx);
-                record = EventBusService.createRecord("iudx.rs.authentication.service", // The service name
-                        "iudx.rs.authentication.service", // the service address,
-                        AuthenticationService.class // the service interface
-                );
+        discovery = ServiceDiscovery.create(vertx);
+        record = EventBusService.createRecord("iudx.rs.authentication.service", // The service name
+            "iudx.rs.authentication.service", // the service address,
+            AuthenticationService.class // the service interface
+        );
 
-                discovery.publish(record, publishRecordHandler -> {
-                    if (publishRecordHandler.succeeded()) {
-                        Record publishedRecord = publishRecordHandler.result();
-                        logger.info("Publication succeeded " + publishedRecord.toJson());
-                    } else {
-                        logger.info("Publication failed " + publishRecordHandler.result());
-                    }
-                });
-
-            }
-
+        discovery.publish(record, publishRecordHandler -> {
+          if (publishRecordHandler.succeeded()) {
+            Record publishedRecord = publishRecordHandler.result();
+            logger.info("Publication succeeded " + publishedRecord.toJson());
+          } else {
+            logger.info("Publication failed " + publishRecordHandler.result());
+          }
         });
 
-    }
+      }
+
+    });
+
+  }
 }
