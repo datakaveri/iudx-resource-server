8fe9784f1e0f67efbc7882c4a487a33b1dffdaff
swaminathanvasanth <vasanth.rajaraman@datakaveri.org>
2020-08-28 19:44:56 +0530

    Updated Verticle to use postgres client

--- src/main/java/iudx/resource/server/databroker/DataBrokerVerticle.java
+++ src/main/java/iudx/resource/server/databroker/DataBrokerVerticle.java
@@ -9,6 +9,8 @@ import io.vertx.core.logging.LoggerFactory;
 import io.vertx.core.spi.cluster.ClusterManager;
 import io.vertx.ext.web.client.WebClient;
 import io.vertx.ext.web.client.WebClientOptions;
+import io.vertx.pgclient.PgConnectOptions;
+import io.vertx.pgclient.PgPool;
 import io.vertx.rabbitmq.RabbitMQClient;
 import io.vertx.rabbitmq.RabbitMQOptions;
 import io.vertx.servicediscovery.Record;
@@ -16,6 +18,7 @@ import io.vertx.servicediscovery.ServiceDiscovery;
 import io.vertx.servicediscovery.types.EventBusService;
 import io.vertx.serviceproxy.ServiceBinder;
 import io.vertx.spi.cluster.hazelcast.HazelcastClusterManager;
+import io.vertx.sqlclient.PoolOptions;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.Properties;
@@ -58,6 +61,17 @@ public class DataBrokerVerticle extends AbstractVerticle {
   private int networkRecoveryInterval;
   private WebClient webClient;
   private WebClientOptions webConfig;
+  /* Database Properties */
+  private String databaseIP;
+  private int databasePort;
+  private String databaseName;
+  private String databaseUserName;
+  private String databasePassword;
+  private int poolSize;
+  private int databasePoolSize;
+  private PgPool pgclient;
+  private PoolOptions poolOptions;
+  private PgConnectOptions connectOptions;
 
   /**
    * This method is used to start the Verticle. It deploys a verticle in a cluster, registers the
@@ -101,6 +115,12 @@ public class DataBrokerVerticle extends AbstractVerticle {
           requestedChannelMax = Integer.parseInt(properties.getProperty("requestedChannelMax"));
           networkRecoveryInterval =
               Integer.parseInt(properties.getProperty("networkRecoveryInterval"));
+          databaseIP = properties.getProperty("callbackDatabaseIP");
+          databasePort = Integer.parseInt(properties.getProperty("callbackDatabasePort"));
+          databaseName = properties.getProperty("callbackDatabaseName");
+          databaseUserName = properties.getProperty("callbackDatabaseUserName");
+          databasePassword = properties.getProperty("callbackDatabasePassword");
+          poolSize = Integer.parseInt(properties.getProperty("callbackpoolSize"));
 
         } catch (Exception ex) {
 
@@ -139,6 +159,20 @@ public class DataBrokerVerticle extends AbstractVerticle {
 
         webClient = WebClient.create(vertx, webConfig);
 
+        /* Set Connection Object */
+        if (connectOptions == null) {
+          connectOptions = new PgConnectOptions().setPort(databasePort).setHost(databaseIP)
+              .setDatabase(databaseName).setUser(databaseUserName).setPassword(databasePassword);
+        }
+
+        /* Pool options */
+        if (poolOptions == null) {
+          poolOptions = new PoolOptions().setMaxSize(poolSize);
+        }
+
+        /* Create the client pool */
+        pgclient = PgPool.pool(vertx, connectOptions, poolOptions);
+        
         /* Create a Json Object for properties */
 
         JsonObject propObj = new JsonObject();
@@ -146,10 +180,16 @@ public class DataBrokerVerticle extends AbstractVerticle {
         propObj.put("userName", dataBrokerUserName);
         propObj.put("password", dataBrokerPassword);
         propObj.put("vHost", dataBrokerVhost);
+        propObj.put("databaseIP", databaseIP);
+        propObj.put("databasePort", databasePort);
+        propObj.put("databaseName", databaseName);
+        propObj.put("databaseUserName", databaseUserName);
+        propObj.put("databasePassword", databasePassword);
+        propObj.put("databasePoolSize", poolSize);
 
         /* Call the databroker constructor with the RabbitMQ client. */
 
-        databroker = new DataBrokerServiceImpl(client, webClient, propObj);
+        databroker = new DataBrokerServiceImpl(client, webClient, propObj, pgclient);
 
         /* Publish the Data Broker service with the Event Bus against an address. */
 
