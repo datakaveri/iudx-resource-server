70886dfd130ec90c221a6a3cecfe76332e4e1174
kailash-adhikari <kailash.adhikari@india.nec.com>
2020-09-22 14:18:04 +0530

    fixes

--- src/main/java/iudx/resource/server/apiserver/handlers/TokenHandler.java
+++ src/main/java/iudx/resource/server/apiserver/handlers/TokenHandler.java
@@ -1,20 +1,26 @@
 package iudx.resource.server.apiserver.handlers;
 
-import static iudx.resource.server.apiserver.util.Constants.*;
+import static iudx.resource.server.apiserver.util.Constants.APPLICATION_JSON;
+import static iudx.resource.server.apiserver.util.Constants.CONTENT_TYPE;
+import static iudx.resource.server.apiserver.util.Constants.HEADER_TOKEN;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
 import org.apache.http.HttpStatus;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import io.vertx.core.AsyncResult;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.sync.Sync;
 import io.vertx.ext.web.RoutingContext;
 import iudx.resource.server.apiserver.response.ResponseType;
 import iudx.resource.server.authenticator.AuthenticationService;
 
-// TODO : centralized authenticated of every request. need some work
+// TODO : centralized authenticated of every request. need some work and testing
 public class TokenHandler implements Handler<RoutingContext> {
   private static final Logger LOGGER = LogManager.getLogger(TokenHandler.class);
   private final String AUTH_SERVICE_ADDRESS = "iudx.rs.authentication.service";
@@ -26,42 +32,53 @@ public class TokenHandler implements Handler<RoutingContext> {
 
   @Override
   public void handle(RoutingContext event) {
-    HttpServerRequest request=event.request();
+    HttpServerRequest request = event.request();
     JsonObject requestJson = event.getBodyAsJson();
     LOGGER.debug("Info path(): " + request.path());
-    if (request.headers().contains(HEADER_TOKEN)
-        || allowedPublicEndPoints.contains(request.path())) {
-      auth(event.response(), requestJson,
-          new JsonObject().put(HEADER_TOKEN, request.headers().get(HEADER_TOKEN)));
+    String token = request.headers().get(HEADER_TOKEN);
+    String path = request.path();
+
+    if (token != null || isAllowedPublicTip(path)) {
+      if (token == null)
+        token = "public";
+      JsonObject authInfo = new JsonObject().put("apiEndpoint", path).put(HEADER_TOKEN, token);
+      JsonObject authResponse = auth(event.response(), requestJson, authInfo);
+      LOGGER.debug("Auth Info :" + authResponse);
+
+      // next handler
+      event.next();
     } else {
       notAuthorized(event.response());
     }
-    // next handler
-    event.next();
   }
 
   private JsonObject auth(HttpServerResponse response, JsonObject request, JsonObject authInfo) {
     JsonObject resJson = new JsonObject();
-    Vertx vertx = Vertx.vertx();
+    Vertx vertx = Vertx.currentContext().owner();
     authenticator = AuthenticationService.createProxy(vertx, AUTH_SERVICE_ADDRESS);
-    vertx.executeBlocking(blockingCodeHandler->{
-      authenticator.tokenInterospect(request, authInfo, handler -> {
-        blockingCodeHandler.complete();
-      });
-    }, false, resultHandler -> {
-      if (resultHandler.succeeded()) {
 
-      } else {
-        notAuthorized(response);
-      }
-    });
-    return new JsonObject();
+    /*
+     * vertx.<JsonObject>executeBlocking(blockingCodeHandler -> {
+     * authenticator.tokenInterospect(request, authInfo, handler -> { if (handler.succeeded()) {
+     * blockingCodeHandler.complete(handler.result()); } else { notAuthorized(response); } }); },
+     * false, resultHandler -> { if (resultHandler.succeeded()) {
+     * resJson.clear().mergeIn(resultHandler.result()); } else { notAuthorized(response); } });
+     */
+
+    // Sync.awaitResult(handler -> authenticator.tokenInterospect(request, authInfo, handler));
+
+    return resJson;
   }
 
   private void notAuthorized(HttpServerResponse response) {
     response.putHeader(CONTENT_TYPE, APPLICATION_JSON)
         .setStatusCode(ResponseType.fromCode(HttpStatus.SC_UNAUTHORIZED).getCode())
         .end("unauthorized");
+  }
+
+  private boolean isAllowedPublicTip(String tip) {
+    Predicate<String> pathFilter = Pattern.compile("/ngsi-ld/v1/entities/*").asPredicate();
+    return allowedPublicEndPoints.stream().filter(pathFilter).findAny().isPresent();
   }
 
 }
