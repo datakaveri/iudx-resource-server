40afb710719183837a3eec46a23136a56115324f
swaminathanvasanth <vasanth.rajaraman@datakaveri.org>
2020-09-24 01:18:21 +0530

    Added compression

--- src/main/java/iudx/resource/server/apiserver/ApiServerVerticle.java
+++ src/main/java/iudx/resource/server/apiserver/ApiServerVerticle.java
@@ -211,8 +211,9 @@ public class ApiServerVerticle extends AbstractVerticle {
 
     /* Setup the HTTPs server properties, APIs and port. */
 
-    server = vertx.createHttpServer(new HttpServerOptions().setSsl(true)
-        .setKeyStoreOptions(new JksOptions().setPath(keystore).setPassword(keystorePassword)));
+    server = vertx.createHttpServer(
+        new HttpServerOptions().setSsl(true).setCompressionSupported(true).setCompressionLevel(5)
+            .setKeyStoreOptions(new JksOptions().setPath(keystore).setPassword(keystorePassword)));
 
     server.requestHandler(router).listen(port);
 
