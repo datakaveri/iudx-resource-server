8a572ca311031df2d00151a31d5b2cb3e915203a
kailash-adhikari <kailash.adhikari@india.nec.com>
2020-09-25 15:35:15 +0530

    unit test fix

--- src/test/java/iudx/resource/server/apiserver/query/QueryMapperTest.java
+++ src/test/java/iudx/resource/server/apiserver/query/QueryMapperTest.java
@@ -10,7 +10,6 @@ import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
-import iudx.resource.server.apiserver.ApiServerVerticle;
 import iudx.resource.server.apiserver.util.Constants;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -22,132 +21,133 @@ import org.junit.jupiter.api.extension.ExtendWith;
 @ExtendWith(VertxExtension.class)
 public class QueryMapperTest {
 
-	private QueryMapper qm;
+  private QueryMapper qm;
 
-	@BeforeEach
-	public void setup(Vertx vertx, VertxTestContext testContext) {
-		vertx.deployVerticle(new ApiServerVerticle(), testContext.completing());
-		qm = new QueryMapper();
-	}
+  @BeforeEach
+  public void setup(Vertx vertx, VertxTestContext testContext) {
+    qm = new QueryMapper();
+    testContext.completeNow();
+  }
 
-	@Test
-	public void testGetQueryTerms(Vertx vertx, VertxTestContext testContext) {
-		String q = "speed>=300";
-		JsonObject json = qm.getQueryTerms(q);
-		assertEquals("speed", json.getString(Constants.JSON_ATTRIBUTE));
-		assertEquals(">=", json.getString(Constants.JSON_OPERATOR));
-		assertEquals("300", json.getString(Constants.JSON_VALUE));
-		testContext.completeNow();
+  @Test
+  public void testGetQueryTerms(Vertx vertx, VertxTestContext testContext) {
+    String q = "speed>=300";
+    System.out.println(qm);
+    JsonObject json = qm.getQueryTerms(q);
+    assertEquals("speed", json.getString(Constants.JSON_ATTRIBUTE));
+    assertEquals(">=", json.getString(Constants.JSON_OPERATOR));
+    assertEquals("300", json.getString(Constants.JSON_VALUE));
+    testContext.completeNow();
 
-	}
+  }
 
-	@Test
-	public void testToJson(Vertx vertx, VertxTestContext testContext) {
-		MultiMap map = MultiMap.caseInsensitiveMultiMap();
-		map.add(Constants.NGSILDQUERY_ID, "id1");
-		map.add(Constants.NGSILDQUERY_ATTRIBUTE, "attr1");
-		NGSILDQueryParams params = new NGSILDQueryParams(map);
-		System.out.println(qm);
-		JsonObject json = qm.toJson(params, false);
+  @Test
+  public void testToJson(Vertx vertx, VertxTestContext testContext) {
+    MultiMap map = MultiMap.caseInsensitiveMultiMap();
+    map.add(Constants.NGSILDQUERY_ID, "id1");
+    map.add(Constants.NGSILDQUERY_ATTRIBUTE, "attr1");
+    NGSILDQueryParams params = new NGSILDQueryParams(map);
+    System.out.println(qm);
+    JsonObject json = qm.toJson(params, false);
 
-		assertTrue(json.containsKey(Constants.NGSILDQUERY_ID));
-		assertTrue(json.containsKey(Constants.NGSILDQUERY_ATTRIBUTE));
-		assertTrue(json.getJsonArray(Constants.NGSILDQUERY_ID) instanceof JsonArray);
-		assertTrue(json.getJsonArray(Constants.NGSILDQUERY_ATTRIBUTE) instanceof JsonArray);
-		testContext.completeNow();
-	}
+    assertTrue(json.containsKey(Constants.NGSILDQUERY_ID));
+    assertTrue(json.containsKey(Constants.NGSILDQUERY_ATTRIBUTE));
+    assertTrue(json.getJsonArray(Constants.NGSILDQUERY_ID) instanceof JsonArray);
+    assertTrue(json.getJsonArray(Constants.NGSILDQUERY_ATTRIBUTE) instanceof JsonArray);
+    testContext.completeNow();
+  }
 
-	@Test
-	@Description("QueryMapper test for Circle geo-query")
-	public void testToJson4CircleQuery(Vertx vertx, VertxTestContext testContext) {
-		// georel=near;maxDistance==360&geometry=Point&coordinates=%5B8.684783577919006%2C49.406131991436396%5D
-		MultiMap map = MultiMap.caseInsensitiveMultiMap();
-		map.add(Constants.NGSILDQUERY_ID, "id1");
-		map.add(Constants.NGSILDQUERY_ATTRIBUTE, "attr1");
-		map.add(Constants.NGSILDQUERY_GEOREL, "near;maxdistance=360");
-		map.add(Constants.NGSILDQUERY_GEOMETRY, "point");
-		map.add(Constants.NGSILDQUERY_COORDINATES, "[8.684628009796143,49.406062179606515]");
-		NGSILDQueryParams params = new NGSILDQueryParams(map);
+  @Test
+  @Description("QueryMapper test for Circle geo-query")
+  public void testToJson4CircleQuery(Vertx vertx, VertxTestContext testContext) {
+    // georel=near;maxDistance==360&geometry=Point&coordinates=%5B8.684783577919006%2C49.406131991436396%5D
+    MultiMap map = MultiMap.caseInsensitiveMultiMap();
+    map.add(Constants.NGSILDQUERY_ID, "id1");
+    map.add(Constants.NGSILDQUERY_ATTRIBUTE, "attr1");
+    map.add(Constants.NGSILDQUERY_GEOREL, "near;maxdistance=360");
+    map.add(Constants.NGSILDQUERY_GEOMETRY, "point");
+    map.add(Constants.NGSILDQUERY_COORDINATES, "[8.684628009796143,49.406062179606515]");
+    NGSILDQueryParams params = new NGSILDQueryParams(map);
 
-		JsonObject json = qm.toJson(params, false);
+    JsonObject json = qm.toJson(params, false);
 
-		assertTrue(json.containsKey(Constants.JSON_LAT));
-		assertTrue(json.containsKey(Constants.JSON_LON));
-		assertTrue(json.containsKey(Constants.JSON_RADIUS));
-		assertFalse(json.containsKey(Constants.NGSILDQUERY_TIMEREL));
-		assertFalse(json.containsKey(Constants.NGSILDQUERY_TIME));
-		assertFalse(json.containsKey(Constants.NGSILDQUERY_ENDTIME));
-		testContext.completeNow();
-	}
+    assertTrue(json.containsKey(Constants.JSON_LAT));
+    assertTrue(json.containsKey(Constants.JSON_LON));
+    assertTrue(json.containsKey(Constants.JSON_RADIUS));
+    assertFalse(json.containsKey(Constants.NGSILDQUERY_TIMEREL));
+    assertFalse(json.containsKey(Constants.NGSILDQUERY_TIME));
+    assertFalse(json.containsKey(Constants.NGSILDQUERY_ENDTIME));
+    testContext.completeNow();
+  }
 
-	@Test
-	@Description("QueryMapper test for geo-query")
-	public void testToJson4GeoQuery(Vertx vertx, VertxTestContext testContext) {
-		MultiMap map = MultiMap.caseInsensitiveMultiMap();
-		map.add(Constants.NGSILDQUERY_ID, "id1,id2");
-		map.add(Constants.NGSILDQUERY_ATTRIBUTE, "attr1,attr2");
-		map.add(Constants.NGSILDQUERY_GEOREL, "within");
-		map.add(Constants.NGSILDQUERY_GEOMETRY, "Polygon");
-		map.add(Constants.NGSILDQUERY_COORDINATES,
-				"[[[8.684628009796143,49.406062179606515],[8.685507774353027,49.4062262372493],[8.68545413017273,49.40634491690448],[8.684579730033875,49.40617736907259],[8.684628009796143,49.406062179606515]]]");
-		NGSILDQueryParams params = new NGSILDQueryParams(map);
+  @Test
+  @Description("QueryMapper test for geo-query")
+  public void testToJson4GeoQuery(Vertx vertx, VertxTestContext testContext) {
+    MultiMap map = MultiMap.caseInsensitiveMultiMap();
+    map.add(Constants.NGSILDQUERY_ID, "id1,id2");
+    map.add(Constants.NGSILDQUERY_ATTRIBUTE, "attr1,attr2");
+    map.add(Constants.NGSILDQUERY_GEOREL, "within");
+    map.add(Constants.NGSILDQUERY_GEOMETRY, "Polygon");
+    map.add(Constants.NGSILDQUERY_COORDINATES,
+        "[[[8.684628009796143,49.406062179606515],[8.685507774353027,49.4062262372493],[8.68545413017273,49.40634491690448],[8.684579730033875,49.40617736907259],[8.684628009796143,49.406062179606515]]]");
+    NGSILDQueryParams params = new NGSILDQueryParams(map);
 
-		JsonObject json = qm.toJson(params, false);
+    JsonObject json = qm.toJson(params, false);
 
-		assertTrue(json.containsKey(Constants.NGSILDQUERY_ID));
-		assertTrue(json.containsKey(Constants.NGSILDQUERY_ATTRIBUTE));
-		assertTrue(json.containsKey(Constants.NGSILDQUERY_COORDINATES));
-		assertTrue(json.containsKey(Constants.NGSILDQUERY_GEOREL));
-		assertTrue(json.containsKey(Constants.NGSILDQUERY_GEOMETRY));
-		assertFalse(json.containsKey(Constants.NGSILDQUERY_TIMEREL));
-		assertFalse(json.containsKey(Constants.NGSILDQUERY_TIME));
-		assertFalse(json.containsKey(Constants.NGSILDQUERY_ENDTIME));
-		testContext.completeNow();
-	}
+    assertTrue(json.containsKey(Constants.NGSILDQUERY_ID));
+    assertTrue(json.containsKey(Constants.NGSILDQUERY_ATTRIBUTE));
+    assertTrue(json.containsKey(Constants.NGSILDQUERY_COORDINATES));
+    assertTrue(json.containsKey(Constants.NGSILDQUERY_GEOREL));
+    assertTrue(json.containsKey(Constants.NGSILDQUERY_GEOMETRY));
+    assertFalse(json.containsKey(Constants.NGSILDQUERY_TIMEREL));
+    assertFalse(json.containsKey(Constants.NGSILDQUERY_TIME));
+    assertFalse(json.containsKey(Constants.NGSILDQUERY_ENDTIME));
+    testContext.completeNow();
+  }
 
-	@Test
-	@Description("QueryMapper test for temporal queries")
-	public void testToJson4TemporalQuery(Vertx vertx, VertxTestContext testContext) {
-		MultiMap map = MultiMap.caseInsensitiveMultiMap();
-		map.add(Constants.NGSILDQUERY_ID, "id1,id2");
-		map.add(Constants.NGSILDQUERY_ATTRIBUTE, "attr1,attr2");
+  @Test
+  @Description("QueryMapper test for temporal queries")
+  public void testToJson4TemporalQuery(Vertx vertx, VertxTestContext testContext) {
+    MultiMap map = MultiMap.caseInsensitiveMultiMap();
+    map.add(Constants.NGSILDQUERY_ID, "id1,id2");
+    map.add(Constants.NGSILDQUERY_ATTRIBUTE, "attr1,attr2");
     map.add(Constants.NGSILDQUERY_TIMEREL, "during");
-		map.add(Constants.NGSILDQUERY_TIME, "2020-01-23T14:20:00Z");
-		map.add(Constants.NGSILDQUERY_ENDTIME, "2020-01-24T14:40:00Z");
-		NGSILDQueryParams params = new NGSILDQueryParams(map);
+    map.add(Constants.NGSILDQUERY_TIME, "2020-01-23T14:20:00Z");
+    map.add(Constants.NGSILDQUERY_ENDTIME, "2020-01-24T14:40:00Z");
+    NGSILDQueryParams params = new NGSILDQueryParams(map);
 
-		JsonObject json = qm.toJson(params, true);
+    JsonObject json = qm.toJson(params, true);
 
-		assertTrue(json.containsKey(Constants.NGSILDQUERY_ID));
-		assertTrue(json.containsKey(Constants.NGSILDQUERY_ATTRIBUTE));
-		assertTrue(json.containsKey(Constants.NGSILDQUERY_TIMEREL));
-		assertTrue(json.containsKey(Constants.NGSILDQUERY_TIME));
-		assertTrue(json.containsKey(Constants.NGSILDQUERY_ENDTIME));
-		testContext.completeNow();
-	}
+    assertTrue(json.containsKey(Constants.NGSILDQUERY_ID));
+    assertTrue(json.containsKey(Constants.NGSILDQUERY_ATTRIBUTE));
+    assertTrue(json.containsKey(Constants.NGSILDQUERY_TIMEREL));
+    assertTrue(json.containsKey(Constants.NGSILDQUERY_TIME));
+    assertTrue(json.containsKey(Constants.NGSILDQUERY_ENDTIME));
+    testContext.completeNow();
+  }
 
-	@Test
-	@Description("QueryMapper test for simple attribute query")
-	public void testToJson4SimpleAttributeQuery(Vertx vertx, VertxTestContext testContext) {
-		MultiMap map = MultiMap.caseInsensitiveMultiMap();
-		map.add(Constants.NGSILDQUERY_ID, "id1,id2");
-		map.add(Constants.NGSILDQUERY_ATTRIBUTE, "attr1,attr2");
-		map.add(Constants.NGSILDQUERY_Q, "speed>=300;temprature==35");
-		NGSILDQueryParams params = new NGSILDQueryParams(map);
+  @Test
+  @Description("QueryMapper test for simple attribute query")
+  public void testToJson4SimpleAttributeQuery(Vertx vertx, VertxTestContext testContext) {
+    MultiMap map = MultiMap.caseInsensitiveMultiMap();
+    map.add(Constants.NGSILDQUERY_ID, "id1,id2");
+    map.add(Constants.NGSILDQUERY_ATTRIBUTE, "attr1,attr2");
+    map.add(Constants.NGSILDQUERY_Q, "speed>=300;temprature==35");
+    NGSILDQueryParams params = new NGSILDQueryParams(map);
 
-		JsonObject json = qm.toJson(params, false);
+    JsonObject json = qm.toJson(params, false);
 
-		assertTrue(json.containsKey(Constants.NGSILDQUERY_ID));
-		assertTrue(json.containsKey(Constants.NGSILDQUERY_ATTRIBUTE));
-		assertTrue(json.containsKey(Constants.JSON_ATTR_QUERY));
-		assertTrue(json.getJsonArray(Constants.JSON_ATTR_QUERY) instanceof JsonArray);
-		assertEquals(json.getJsonArray(Constants.JSON_ATTR_QUERY).size(), 2);
-		testContext.completeNow();
-	}
+    assertTrue(json.containsKey(Constants.NGSILDQUERY_ID));
+    assertTrue(json.containsKey(Constants.NGSILDQUERY_ATTRIBUTE));
+    assertTrue(json.containsKey(Constants.JSON_ATTR_QUERY));
+    assertTrue(json.getJsonArray(Constants.JSON_ATTR_QUERY) instanceof JsonArray);
+    assertEquals(json.getJsonArray(Constants.JSON_ATTR_QUERY).size(), 2);
+    testContext.completeNow();
+  }
 
-	@AfterEach
-	public void teardown() {
+  @AfterEach
+  public void teardown() {
 
-	}
+  }
 
 }
