f869658737aa098ccf14e78a67cc81dfce8f29c7
Kailash Adhikari <adhikari.kailash88@gmail.com>
2020-12-08 14:57:44 +0530

    postman collection changes

--- src/main/java/iudx/resource/server/apiserver/validation/ValidationFailureHandler.java
+++ src/main/java/iudx/resource/server/apiserver/validation/ValidationFailureHandler.java
@@ -1,10 +1,15 @@
 package iudx.resource.server.apiserver.validation;
 
-import static iudx.resource.server.apiserver.util.Constants.*;
+import static iudx.resource.server.apiserver.util.Constants.APPLICATION_JSON;
+import static iudx.resource.server.apiserver.util.Constants.CONTENT_TYPE;
+import static iudx.resource.server.apiserver.util.Constants.JSON_DETAIL;
+import static iudx.resource.server.apiserver.util.Constants.JSON_TITLE;
+import static iudx.resource.server.apiserver.util.Constants.JSON_TYPE;
 import org.apache.http.HttpStatus;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import io.vertx.core.Handler;
+import io.vertx.core.json.DecodeException;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.api.validation.ValidationException;
@@ -16,7 +21,7 @@ public class ValidationFailureHandler implements Handler<RoutingContext>{
   public void handle(RoutingContext context) {
     Throwable failure = context.failure();
     LOGGER.error("error :"+failure);
-    if (failure instanceof ValidationException) {
+    if (failure instanceof ValidationException) { 
       // Something went wrong during validation!
       String failedParameter=((ValidationException) failure).parameterName();
       LOGGER.error("error :" +failure.getMessage()+" param : "+failedParameter);
@@ -33,7 +38,7 @@ public class ValidationFailureHandler implements Handler<RoutingContext>{
   private JsonObject validationFailureReponse(String message) {
     return new JsonObject()
         .put(JSON_TYPE, HttpStatus.SC_BAD_REQUEST)
-        .put(JSON_TITLE, "Bad request")
+        .put(JSON_TITLE, "Bad Request")
         .put(JSON_DETAIL, message);
   }
 
