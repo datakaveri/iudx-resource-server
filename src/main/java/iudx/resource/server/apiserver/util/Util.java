8a6e548954a9377c4763afa612ed0bb056e7d1af
Kailash Adhikari <adhikari.kailash88@gmail.com>
2021-02-25 12:53:26 +0530

    patch for multiple id in token interospect

--- src/main/java/iudx/resource/server/apiserver/util/Util.java
+++ src/main/java/iudx/resource/server/apiserver/util/Util.java
@@ -2,9 +2,20 @@ package iudx.resource.server.apiserver.util;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.List;
 import java.util.function.Function;
+import io.vertx.core.json.JsonArray;
 
 public class Util {
+  
+  public static <T> List<T> toList(JsonArray arr) {
+    if (arr == null) {
+      return null;
+    } else {
+      return (List<T>) arr.getList();
+    }
+  }
+  
 
   public static Function<String, URI> toUriFunction = (value) -> {
     URI uri = null;
