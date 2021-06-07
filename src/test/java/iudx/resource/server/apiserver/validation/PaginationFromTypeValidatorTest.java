9ab27efbe0b0b837aaa69b1706277e64247186bf
Kailash Adhikari <adhikari.kailash88@gmail.com>
2021-06-07 11:53:06 +0530

    junit for pagination

---
+++ src/test/java/iudx/resource/server/apiserver/validation/PaginationFromTypeValidatorTest.java
@@ -0,0 +1,68 @@
+package iudx.resource.server.apiserver.validation;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import io.vertx.core.Vertx;
+import io.vertx.core.cli.annotations.Description;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import iudx.resource.server.apiserver.validation.types.PaginationFromTypeValidator;
+
+@ExtendWith(VertxExtension.class)
+public class PaginationFromTypeValidatorTest {
+
+  private PaginationFromTypeValidator paginationFromTypeValidator;
+  
+  @BeforeEach
+  public void setup(Vertx vertx, VertxTestContext testContext) {
+    testContext.completeNow();
+  }
+  
+  static Stream<Arguments> allowedValues() {
+    // Add any valid value which will pass successfully.
+    return Stream.of(
+        Arguments.of(null, false),
+        Arguments.of("1000", false),
+        Arguments.of("5000", false),
+        Arguments.of("2500", false),
+        Arguments.of("0", false));
+  }
+  
+  @ParameterizedTest
+  @MethodSource("allowedValues")
+  @Description("pagination from type parameter allowed values.")
+  public void testValidFromTypeValue(String value, boolean required, Vertx vertx,
+      VertxTestContext testContext) {
+    paginationFromTypeValidator = new PaginationFromTypeValidator(value, required);
+    assertTrue(paginationFromTypeValidator.isValid());
+    testContext.completeNow();
+  }
+  
+  
+  static Stream<Arguments> invalidValues() {
+    // Add any valid value which will pass successfully.
+    return Stream.of(
+        Arguments.of("-1", false),
+        Arguments.of("50001", false),
+        Arguments.of("   ", false),
+        Arguments.of("7896541233568796313611634", false),
+        Arguments.of("false", false),
+        Arguments.of("kajlksdjloasknfdlkanslodnmalsdasd", false));
+  }
+  
+  @ParameterizedTest
+  @MethodSource("invalidValues")
+  @Description("pagination from type parameter invalid values.")
+  public void testInvalidFromTypeValue(String value, boolean required, Vertx vertx,
+      VertxTestContext testContext) {
+    paginationFromTypeValidator = new PaginationFromTypeValidator(value, required);
+    assertFalse(paginationFromTypeValidator.isValid());
+    testContext.completeNow();
+  }
+}
