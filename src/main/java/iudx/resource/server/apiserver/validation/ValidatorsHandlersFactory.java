b6529138195b545fad470debeb6571e38ef22841
Kailash Adhikari <adhikari.kailash88@gmail.com>
2021-07-07 13:57:12 +0530

    use constants instead of literals

--- src/main/java/iudx/resource/server/apiserver/validation/ValidatorsHandlersFactory.java
+++ src/main/java/iudx/resource/server/apiserver/validation/ValidatorsHandlersFactory.java
@@ -126,11 +126,11 @@ public class ValidatorsHandlersFactory {
       final MultiMap headers) {
     
     List<Validator> validators = new ArrayList<>();
-    validators.add(new StringTypeValidator(parameters.get("domain"), true,ID_DOMAIN_REGEX));
-    validators.add(new StringTypeValidator(parameters.get("userSha"), true,ID_USERSHA_REGEX));
-    validators.add(new StringTypeValidator(parameters.get("resourceServer"), true,ID_RS_REGEX));
-    validators.add(new StringTypeValidator(parameters.get("resourceGroup"), true,ID_RG_REGEX));
-    validators.add(new StringTypeValidator(parameters.get("resourceName"), true,ID_RN_REGEX));
+    validators.add(new StringTypeValidator(parameters.get(DOMAIN), true,ID_DOMAIN_REGEX));
+    validators.add(new StringTypeValidator(parameters.get(USERSHA), true,ID_USERSHA_REGEX));
+    validators.add(new StringTypeValidator(parameters.get(RESOURCE_SERVER), true,ID_RS_REGEX));
+    validators.add(new StringTypeValidator(parameters.get(RESOURCE_GROUP), true,ID_RG_REGEX));
+    validators.add(new StringTypeValidator(parameters.get(RESOURCE_NAME), true,ID_RN_REGEX));
 
     return validators;
   }
