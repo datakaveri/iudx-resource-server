d7630356900bad296699ebc4acbf99321e20558a
Kailash Adhikari <adhikari.kailash88@gmail.com>
2021-09-06 12:11:36 +0530

    latest verticle redis changes

---
+++ src/main/java/iudx/resource/server/database/latest/RedisArgs.java
@@ -0,0 +1,23 @@
+package iudx.resource.server.database.latest;
+
+public class RedisArgs {
+  
+  private String Key;
+  private String path;
+  
+  public String getKey() {
+    return Key;
+  }
+  public void setKey(String key) {
+    Key = key;
+  }
+  public String getPath() {
+    return path;
+  }
+  public void setPath(String path) {
+    this.path = path;
+  }
+  
+  
+
+}
