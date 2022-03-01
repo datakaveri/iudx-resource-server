637d4651c4a86d7a7576dde251c3ade70f8f7818
Pranav Doshetty <pranav.doshetty@datakaveri.org>
2022-03-01 23:18:35 +0530

    junit test cases

--- src/test/java/iudx/resource/server/database/async/AsyncServiceTest.java
+++ src/test/java/iudx/resource/server/database/async/AsyncServiceTest.java
@@ -83,7 +83,7 @@ public class AsyncServiceTest {
     asyncServiceSpy = spy(asyncService);
 
     asyncResult = mock(AsyncResult.class);
-    when(asyncResult.succeeded()).thenReturn(false);
+//    when(asyncResult.succeeded()).thenReturn(false);
 
     URL url = new URL("https://www.example.com");
     when(fileOpsHelper.generatePreSignedUrl(anyLong(), any())).thenReturn(url);
@@ -135,20 +135,33 @@ public class AsyncServiceTest {
     testContext.completeNow();
   }
 
+  public JsonObject query() {
+    JsonObject query = new JsonObject()
+        .put(
+            "id",
+            new JsonArray()
+                .add(
+                    "iisc.ac.in/89a36273d77dac4cf38114fca1bbe64392547f86/rs.iudx.io/pune-env-flood/FWR055"))
+        .put("time", "2020-10-10T14:20:00Z")
+        .put("endtime", "2020-10-20T14:20:00Z")
+        .put("timerel", "during")
+        .put("searchType", "temporalSearch");
+
+    return query;
+  }
+
   public JsonArray record() {
     JsonArray record = new JsonArray();
     record.add(
         new JsonObject()
-            .put(
-                "id",
-                new JsonArray()
-                    .add(
-                        "iisc.ac.in/89a36273d77dac4cf38114fca1bbe64392547f86/rs.iudx.io/pune-env-flood/FWR055"))
-            .put("time", "2020-10-10T14:20:00Z")
-            .put("endtime", "2020-10-20T14:20:00Z")
-            .put("timerel", "during")
-            .put("searchType", "temporalSearch")
-            .put(OBJECT_ID, "b8a47206-364c-4580-8885-45205118db57"));
+            .put("_id","4c030b19-4954-4e56-868a-c36d80a77902")
+            .put("search_id","4b25aa92-47bb-4c91-98c0-47a1c7a51fbe")
+            .put("request_id","efb0b92cd5b50d0a75a939ffa997c6e4fccdc62414ad0177a020eec98f69144e")
+            .put("status","COMPLETE")
+            .put("s3_url","https://example.com")
+            .put("expiry","2022-03-02T16:08:38.495665")
+            .put("user_id","15c7506f-c800-48d6-adeb-0542b03947c6")
+            .put("object_id", "b8a47206-364c-4580-8885-45205118db57"));
 
     return record;
   }
@@ -161,7 +174,7 @@ public class AsyncServiceTest {
     String sub = "15c7506f-c800-48d6-adeb-0542b03947c6";
     String searchId = "18cc743b-59a4-4c26-9f54-e243986ed709";
     JsonArray record = record();
-    JsonObject query = record.getJsonObject(0);
+    JsonObject query = query();
 
     doAnswer(Answer -> Future.succeededFuture(record))
         .when(asyncServiceSpy)
@@ -183,13 +196,11 @@ public class AsyncServiceTest {
     String requestId = "efb0b92cd5b50d0a75a939ffa997c6e4fccdc62414ad0177a020eec98f69144e";
     String sub = "15c7506f-c800-48d6-adeb-0542b03947c6";
     String searchId = "4b25aa92-47bb-4c91-98c0-47a1c7a51fbe";
-    JsonArray record = record();
-    JsonObject query = record.getJsonObject(0);
+    JsonObject query = query();
 
     doAnswer(Answer -> Future.failedFuture("record doesn't exist"))
         .when(asyncServiceSpy)
         .getRecord4RequestId(any());
-    doAnswer(Answer -> Future.succeededFuture()).when(asyncServiceSpy).executePGQuery(any());
 
     when(asyncResult.succeeded()).thenReturn(true);
 
@@ -201,5 +212,25 @@ public class AsyncServiceTest {
     verify(fileOpsHelper, times(1)).generatePreSignedUrl(anyLong(), any());
     verify(asyncServiceSpy, times(2)).executePGQuery(any());
     testContext.completeNow();
+  }
+
+  @Test
+  @DisplayName("success - async status")
+  public void successfulAsyncSearch(VertxTestContext testContext) {
+
+    String sub = "15c7506f-c800-48d6-adeb-0542b03947c6";
+    String searchId = "4b25aa92-47bb-4c91-98c0-47a1c7a51fbe";
+    JsonArray record = record();
+
+    when(asyncResult.succeeded()).thenReturn(true);
+    when(asyncResult.result()).thenReturn(new JsonObject().put("result",record));
+
+    asyncServiceSpy.asyncStatus(sub,searchId,handler -> {
+      if(handler.succeeded()) {
+        testContext.completeNow();
+      } else {
+        testContext.failNow("fail");
+      }
+    });
   }
 }
