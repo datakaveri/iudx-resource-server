4eec4500dc16157bbe55bced1cffe9bcf0057aee
Kailash Adhikari <adhikari.kailash88@gmail.com>
2022-03-03 13:10:07 +0530

    flyway for RS

--- src/main/resources/db/migration/V1__init-tables.sql
+++ src/main/resources/db/migration/V1__init-tables.sql
@@ -1,5 +1,8 @@
 -- random uuid extension for primary key.
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+ALTER SCHEMA ${flyway:defaultSchema} OWNER TO ${flyway:user};
+
 -- constants enum
 -- subscription types
 CREATE TYPE sub_type AS ENUM
@@ -7,14 +10,18 @@ CREATE TYPE sub_type AS ENUM
    'STREAMING',
    'CALLBACK'
 );
+
 CREATE type Query_Progress as ENUM
 (
    'IN_PROGRESS',
    'ERROR',
    'COMPLETE'
 )
+
+
+---
 -- Token invalidation table
--- TODO: decide max length for varchar
+---
 CREATE TABLE IF NOT EXISTS revoked_tokens
 (
    _id uuid NOT NULL,
@@ -23,7 +30,12 @@ CREATE TABLE IF NOT EXISTS revoked_tokens
    modified_at timestamp without time zone NOT NULL,
    CONSTRAINT revoke_tokens_pk PRIMARY KEY (_id)
 );
+
+ALTER TABLE revoked_tokens OWNER TO ${flyway:user};
+
+---
 -- Unique attribute table
+---
 CREATE TABLE IF NOT EXISTS unique_attributes
 (
    _id uuid DEFAULT uuid_generate_v4 () NOT NULL,
@@ -34,7 +46,12 @@ CREATE TABLE IF NOT EXISTS unique_attributes
    CONSTRAINT unique_attrib_pk PRIMARY KEY (_id),
    CONSTRAINT resource_id_unique UNIQUE (resource_id)
 );
+
+ALTER TABLE unique_attributes OWNER TO ${flyway:user};
+
+---
 -- subscription table
+---
 CREATE TABLE IF NOT EXISTS subscriptions
 (
    _id varchar NOT NULL,
@@ -51,7 +68,10 @@ CREATE TABLE IF NOT EXISTS subscriptions
       entity
    )
 );
+
+---
 -- s3 URL table
+---
 CREATE TABLE IF NOT EXISTS s3_upload_url
 (
    _id uuid NOT NULL,
@@ -66,7 +86,28 @@ CREATE TABLE IF NOT EXISTS s3_upload_url
    modified_at timestamp without time zone NOT NULL,
    CONSTRAINT upload_url_pk PRIMARY KEY (_id)
 );
--- Functions for audit[new,update] on table/column
+
+ALTER TABLE subscriptions OWNER TO ${flyway:user};
+
+---
+-- databroker table
+---
+CREATE TABLE IF NOT EXISTS databroker
+(
+   username character varying (255) NOT NULL,
+   password character varying (50) NOT NULL,
+   created_at timestamp without time zone NOT NULL,
+   modified_at timestamp without time zone NOT NULL,
+   CONSTRAINT databroker_pkey PRIMARY KEY (username),
+   CONSTRAINT databroker_username_key UNIQUE (username)
+);
+
+ALTER TABLE databroker OWNER TO ${flyway:user};
+
+---
+-- Functions for audit[cerated,updated] on table/column
+---
+
 -- modified_at column function
 CREATE
 OR REPLACE
@@ -75,6 +116,7 @@ BEGIN NEW.modified_at = now ();
 RETURN NEW;
 END;
 $$ language 'plpgsql';
+
 -- created_at column function
 CREATE
 OR REPLACE
@@ -83,24 +125,53 @@ BEGIN NEW.created_at = now ();
 RETURN NEW;
 END;
 $$ language 'plpgsql';
--- Triggers for audit
+
+---
+-- Triggers
+---
+
 -- unique attribute table
+CREATE TRIGGER update_ua_created BEFORE INSERT ON unique_attributes FOR EACH ROW EXECUTE PROCEDURE update_created ();
 CREATE TRIGGER update_ua_modified BEFORE INSERT
 OR UPDATE ON
    unique_attributes FOR EACH ROW EXECUTE PROCEDURE update_modified ();
-CREATE TRIGGER update_ua_created BEFORE INSERT ON unique_attributes FOR EACH ROW EXECUTE PROCEDURE update_created ();
+   
+
 -- revoked_tokens table
 CREATE TRIGGER update_rt_created BEFORE INSERT ON revoked_tokens FOR EACH ROW EXECUTE PROCEDURE update_created ();
 CREATE TRIGGER update_rt_modified BEFORE INSERT
 OR UPDATE ON
    revoked_tokens FOR EACH ROW EXECUTE PROCEDURE update_modified ();
+
+
 --subscription table
 CREATE TRIGGER update_sub_created BEFORE INSERT ON subscriptions FOR EACH ROW EXECUTE PROCEDURE update_created ();
 CREATE TRIGGER update_sub_modified BEFORE INSERT
 OR UPDATE ON
    subscriptions FOR EACH ROW EXECUTE PROCEDURE update_modified ();
+
 -- s3_upload_url
 CREATE TRIGGER update_s3_url_created BEFORE INSERT ON s3_upload_url FOR EACH ROW EXECUTE PROCEDURE update_created ();
 CREATE TRIGGER update_s3_url_modified BEFORE INSERT
 OR UPDATE ON
    s3_upload_url FOR EACH ROW EXECUTE PROCEDURE update_modified ();
+
+
+--databroker table
+CREATE TRIGGER update_user_created BEFORE INSERT ON databroker FOR EACH ROW EXECUTE PROCEDURE update_created ();
+CREATE TRIGGER update_user_modified BEFORE INSERT
+OR UPDATE ON
+   databroker FOR EACH ROW EXECUTE PROCEDURE update_modified ();
+   
+   
+ ---  
+ -- grants
+ ---
+ 
+ GRANT USAGE ON SCHEMA ${flyway:defaultSchema} TO ${rsUser};
+ 
+ GRANT SELECT,INSERT,UPDATE,DELETE ON TABLE revoked_tokens TO ${rsUser};
+ GRANT SELECT,INSERT,UPDATE,DELETE ON TABLE unique_attributes TO ${rsUser};
+ GRANT SELECT,INSERT,UPDATE,DELETE ON TABLE subscriptions TO ${rsUser};
+ GRANT SELECT,INSERT,UPDATE,DELETE ON TABLE databroker TO ${rsUser};
+ 
