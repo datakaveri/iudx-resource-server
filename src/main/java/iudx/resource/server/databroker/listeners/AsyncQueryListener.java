c447590fe4876b46b344130c252c4b6a9cd20d4a
Shreelakshmi M Joshi <shreelakshmi.joshi@outlook.com>
2022-07-19 17:58:56 +0530

    Create test classes

--- src/main/java/iudx/resource/server/databroker/listeners/AsyncQueryListener.java
+++ src/main/java/iudx/resource/server/databroker/listeners/AsyncQueryListener.java
@@ -1,6 +1,8 @@
 package iudx.resource.server.databroker.listeners;
 
 import static iudx.resource.server.common.Constants.ASYNC_QUERY_Q;
+
+import io.vertx.core.Future;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import io.vertx.core.Vertx;
@@ -16,7 +18,7 @@ public class AsyncQueryListener implements RMQListeners {
 
   private static final Logger LOGGER = LogManager.getLogger(AsyncQueryListener.class);
 
-  private final RabbitMQClient client;
+  RabbitMQClient client;
 
   private final QueueOptions options = new QueueOptions()
       .setMaxInternalQueueSize(2)
@@ -33,6 +35,36 @@ public class AsyncQueryListener implements RMQListeners {
 
   @Override
   public void start() {
+    Future<Void> future = client.start();
+    if (future.succeeded()) {
+      LOGGER.trace("starting Q listener for Async query");
+      client.basicConsumer(ASYNC_QUERY_Q, options, asyncQListenerHandler -> {
+        if (asyncQListenerHandler.succeeded()) {
+          RabbitMQConsumer mqConsumer = asyncQListenerHandler.result();
+          mqConsumer.handler(message -> {
+            Buffer body = message.body();
+            if (body != null) {
+              JsonObject asyncQueryJson = new JsonObject(body);
+              LOGGER.debug("received message from async-query Q :" + asyncQueryJson);
+              String requestId = asyncQueryJson.getString("requestId");
+              String searchId = asyncQueryJson.getString("searchId");
+              String user = asyncQueryJson.getString("sub");
+              JsonObject query = asyncQueryJson.getJsonObject("query");
+              LOGGER.debug("query received from RMQ : {}", query);
+
+              asyncService.asyncSearch(requestId, user, searchId, query);
+            } else {
+              LOGGER.error("Empty json received from async query queue");
+            }
+          });
+        }
+      });
+    }
+    else
+    {
+      LOGGER.error("Rabbit client startup failed." + future.cause().getMessage());
+    }
+    /**
     client
         .start()
         .onSuccess(handler -> {
@@ -63,7 +95,8 @@ public class AsyncQueryListener implements RMQListeners {
         .onFailure(handler -> {
           LOGGER.error("Rabbit client startup failed." + handler);
         });
-
+     **/
   }
+
 
 }
