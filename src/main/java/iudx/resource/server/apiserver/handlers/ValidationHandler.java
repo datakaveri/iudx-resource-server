979680de4a51f05f5930cef3fc4828e96e442160
Kailash Adhikari <adhikari.kailash88@gmail.com>
2022-10-17 11:37:34 +0530

    post api bug

--- src/main/java/iudx/resource/server/apiserver/handlers/ValidationHandler.java
+++ src/main/java/iudx/resource/server/apiserver/handlers/ValidationHandler.java
@@ -4,13 +4,13 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import io.vertx.core.Handler;
 import io.vertx.core.MultiMap;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.RequestBody;
 import io.vertx.ext.web.RoutingContext;
 import iudx.resource.server.apiserver.util.RequestType;
 import iudx.resource.server.apiserver.validation.ValidatorsHandlersFactory;
@@ -33,13 +33,20 @@ public class ValidationHandler implements Handler<RoutingContext> {
     ValidatorsHandlersFactory validationFactory = new ValidatorsHandlersFactory();
     MultiMap parameters = context.request().params();
     MultiMap headers = context.request().headers();
-    JsonObject body = context.getBodyAsJson();
+    RequestBody requestBody=context.body();
+    JsonObject body=null;
+    if(requestBody!=null) {
+      if(requestBody.asJsonObject()!=null) {
+        body=requestBody.asJsonObject().copy();
+      }
+    }
     Map<String, String> pathParams = context.pathParams();
     parameters.addAll(pathParams);
 
     List<Validator> validations = validationFactory.build(vertx, requestType, parameters, headers, body);
     for (Validator validator : Optional.ofNullable(validations).orElse(Collections.emptyList())) {
       LOGGER.debug("validator :" + validator.getClass().getName());
+      validator.isValid();
     }
     context.next();
     return;
