147133e21e66f9bbba846f3cbef2d5cbc96d0219
ankitmashu <ankitmashu@gmail.com>
2023-02-13 12:07:29 +0530

    schema change in auditing_rs

--- src/main/resources/db/migration/V4_3__auditingrs-add-column.sql
+++ src/main/resources/db/migration/V4_3__auditingrs-add-column.sql
@@ -1,2 +1,1 @@
-ALTER TABLE auditing_rs ADD COLUMN time timestamp without time zone
-UPDATE auditing_rs set time = split_part(isotime,'[',1)::timestamp with time zone at time zone 'UTC'
+ALTER TABLE auditing_rs ADD COLUMN time timestamp without time zone;
