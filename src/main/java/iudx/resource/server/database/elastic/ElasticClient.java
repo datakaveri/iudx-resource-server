d40abe079452a0d342c651fe8a48ebd1a8d07904
shreelakshmijoshi <shreelakshmi.joshi@datakaveri.org>
2023-04-26 09:25:47 +0530

    Convert ES response to CSV

--- src/main/java/iudx/resource/server/database/elastic/ElasticClient.java
+++ src/main/java/iudx/resource/server/database/elastic/ElasticClient.java
@@ -7,7 +7,11 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
+
+import com.fasterxml.jackson.databind.JsonNode;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
@@ -97,6 +101,12 @@ public class ElasticClient {
 
         List<Hit<ObjectNode>> searchHits = response.hits().hits();
 
+        long startTime = System.currentTimeMillis();
+        getHeader(searchHits);
+        appendToFile(searchHits);
+        long endTime = System.currentTimeMillis();
+        LOGGER.debug("Time Taken in milliseconds: {} ", endTime - startTime);
+
         LOGGER.debug(file.getAbsolutePath());
 
         FileWriter filew = new FileWriter(file);
@@ -147,6 +157,74 @@ public class ElasticClient {
     return promise.future();
   }
 
+  private void appendToFile(List<Hit<ObjectNode>> searchHits) {
+    for (Hit hit : searchHits) {
+      Map<String, Object> map = new JsonFlatten((JsonNode) hit.source()).flatten();
+//          LOGGER.debug("Map : " + map);
+//          LOGGER.debug("Field names | header : "+ map.keySet());
+      //         make this as the csv header
+
+      Set<String> header = map.keySet();
+      appendToCSVFile(map, header);
+    }
+  }
+
+  private void getHeader(List<Hit<ObjectNode>> searchHits) {
+    for (Hit hit : searchHits) {
+      Map<String, Object> map = new JsonFlatten((JsonNode) hit.source()).flatten();
+      Set<String> header = map.keySet();
+      simpleFileWriter(header, "something.csv");
+      break;
+    }
+  }
+
+  private void appendToCSVFile(Map<String, Object> map, Set<String> header) {
+    FileWriter fileWriter = null;
+    try {
+      fileWriter = new FileWriter("something.csv", true);
+      StringBuilder stringBuilder = new StringBuilder();
+
+//      LOGGER.debug("map.entrySet() : " + map.entrySet());
+      for (String field : header) {
+        var cell = map.get(field);
+        if (cell == null) {
+          stringBuilder.append("" + ",");
+        } else {
+          stringBuilder.append(cell + ",");
+        }
+//        LOGGER.debug("map.get(field) : " + map.get(field));
+      }
+
+
+      String row = stringBuilder.substring(0, stringBuilder.length() - 1);
+//      LOGGER.debug("ROW : " + row);
+      fileWriter.append(row).append("\n");
+
+      fileWriter.close();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+
+  }
+
+
+  // for writing headers
+  private void simpleFileWriter(Set<String> header, String fileName) {
+    FileWriter fileWriter = null;
+    try {
+      fileWriter = new FileWriter(fileName);
+      StringBuilder stringBuilder = new StringBuilder();
+      for (String obj : header) {
+        stringBuilder.append(obj + ",");
+      }
+      String data = stringBuilder.substring(0, stringBuilder.length() - 1);
+      fileWriter.write(data + "\n");
+
+      fileWriter.close();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
   private ScrollRequest nextScrollRequest(final String scrollId) {
     return ScrollRequest
         .of(scrollRequest -> scrollRequest.scrollId(scrollId).scroll(Time.of(t -> t.time("5m"))));
---
+++ src/main/java/iudx/resource/server/database/elastic/JsonFlatten.java
@@ -0,0 +1,65 @@
+package iudx.resource.server.database.elastic;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.node.ValueNode;
+import com.github.sisyphsu.dateparser.DateParserUtils;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class JsonFlatten {
+
+    private final Map<String, ValueNode> json = new LinkedHashMap<>();
+    private final LinkedHashMap<String, Object> jsonObj = new LinkedHashMap<>();
+    private final JsonNode root;
+
+    public JsonFlatten(JsonNode node) {
+        this.root = Objects.requireNonNull(node);
+    }
+
+    public static void flattenJson(JsonNode node, String parent, Map<String, ValueNode> map) {
+        if (node instanceof ValueNode) {
+            map.put(parent, (ValueNode) node);
+        } else {
+            String prefix = parent == null ? "" : parent + ".";
+            if (node instanceof ArrayNode) {
+                ArrayNode arrayNode = (ArrayNode) node;
+                for (int i = 0; i < arrayNode.size(); i++) {
+                    flattenJson(arrayNode.get(i), prefix + i, map);
+                }
+            } else if (node instanceof ObjectNode) {
+                ObjectNode objectNode = (ObjectNode) node;
+                for (Iterator<Map.Entry<String, JsonNode>> it = objectNode.fields(); it.hasNext(); ) {
+                    Map.Entry<String, JsonNode> field = it.next();
+                    flattenJson(field.getValue(), prefix + field.getKey(), map);
+                }
+            } else {
+                throw new RuntimeException("unknown json node");
+            }
+        }
+    }
+
+    public LinkedHashMap<String, Object> flatten() {
+        flattenJson(root, null, json);
+        for (String key : json.keySet()) {
+            if (Objects.equals(key, "observationDateTime")) {
+                LocalDateTime localDateTime = DateParserUtils.parseDateTime(json.get(key).asText());
+                jsonObj.put(key, Timestamp.valueOf(localDateTime));
+                continue;
+            }
+            if (json.get(key).isInt()) jsonObj.put(key, json.get(key).asInt());
+            if (json.get(key).isLong()) jsonObj.put(key, json.get(key).asLong());
+            if (json.get(key).isFloat()) jsonObj.put(key, json.get(key).asDouble());
+            if (json.get(key).isDouble()) jsonObj.put(key, json.get(key).asDouble());
+            if (json.get(key).isBoolean()) jsonObj.put(key, json.get(key).asBoolean());
+            if (json.get(key).isTextual()) jsonObj.put(key, json.get(key).asText());
+        }
+        return jsonObj;
+    }
+}
