6785965bd568a4f944925b3ef89b215932379872
ananjaykumar2 <ananjaykumar2@gmail.com>
2023-05-16 11:47:14 +0530

    create user table flyway schema

--- src/main/resources/db/migration/V5_0__create-user-table.sql
+++ src/main/resources/db/migration/V5_0__create-user-table.sql
@@ -7,14 +7,14 @@ CREATE type status_type as ENUM
 
 CREATE TABLE IF NOT EXISTS user_table
 (
-    primaryKey uuid NOT NULL,
-    user_id uuid NOT NULL,
+    _id uuid DEFAULT uuid_generate_v4 () NOT NULL,
+    userid uuid NOT NULL,
     role varchar NOT NULL,
     status status_type NOT NULL,
-    payload json not null,
+    request_json json not null,
     created_at timestamp without time zone NOT NULL,
     modified_at timestamp without time zone NOT NULL,
-    CONSTRAINT primaryKey_pk PRIMARY KEY (primaryKey),
+    CONSTRAINT id_pk PRIMARY KEY (_id)
 );
 
 CREATE TRIGGER update_user_table_created BEFORE INSERT ON user_table FOR EACH ROW EXECUTE PROCEDURE update_created ();
