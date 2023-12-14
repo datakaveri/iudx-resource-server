c9c51ef116bde00a18f9ba3e24f61030ad48769e
Kranthi-Guribilli <kranthi.guribilli@datakaveri.org>
2023-12-14 10:57:07 +0530

    Add RestAssured integration tests for Reset Password API in the Resource Server.

---
+++ src/test/java/iudx/resource/server/apiserver/integrationTests/resetPasswordAPI/ResetPasswordAPIsIT.java
@@ -0,0 +1,75 @@
+package iudx.resource.server.apiserver.integrationTests.resetPasswordAPI;
+
+import io.restassured.response.Response;
+import iudx.resource.server.apiserver.integrationTests.RestAssuredConfiguration;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static io.restassured.RestAssured.given;
+import static iudx.resource.server.authenticator.JwtTokenHelper.secureResourceToken;
+import static org.hamcrest.Matchers.equalTo;
+
+/**
+ * Integration tests for the Reset Password APIs in the Resource Server.
+ * These tests cover various scenarios such as resetting user passwords with different HTTP response codes.
+ * The tests include cases for successful password reset (HTTP 200), password reset on a non-existent endpoint (HTTP 404),
+ * and unauthorized password reset attempt (HTTP 401).
+ */
+@ExtendWith(RestAssuredConfiguration.class)
+public class ResetPasswordAPIsIT {
+    private static final Logger LOGGER = LogManager.getLogger(ResetPasswordAPIsIT.class);
+
+    @Test
+    @DisplayName("testing Reset Password  - 200 (Success) Reset user password")
+    void ResetUserPwd() {
+        Response response = given()
+                .header("token", secureResourceToken)
+                .contentType("application/json")
+                .when()
+                .post("/user/resetPassword")
+                .then()
+                .statusCode(200)
+                .body("title", equalTo("Successfully changed the password"))
+                .extract()
+                .response();
+        //Log the entire response details
+        LOGGER.debug("Response details:\n" + response.prettyPrint());
+    }
+
+    @Test
+    @DisplayName("testing Reset Password  - 404 (Not Found) Reset user password")
+    void ResetUserPwdNotFound() {
+        Response response = given()
+                .header("token", secureResourceToken)
+                .contentType("application/json")
+                .when()
+                .post("/user/resetPasswords")
+                .then()
+                .statusCode(404)
+                .body("title", equalTo("Not Found"))
+                .extract()
+                .response();
+        //Log the entire response details
+        LOGGER.debug("Response details:\n" + response.prettyPrint());
+    }
+
+    @Test
+    @DisplayName("testing Reset Password  - 401 (Not Authorized) Reset user password")
+    void ResetUserPwdUnAuth() {
+        Response response = given()
+                .header("token", "abc")
+                .contentType("application/json")
+                .when()
+                .post("/user/resetPassword")
+                .then()
+                .statusCode(401)
+                .body("title", equalTo("Not Authorized"))
+                .extract()
+                .response();
+        //Log the entire response details
+        LOGGER.debug("Response details:\n" + response.prettyPrint());
+    }
+}
