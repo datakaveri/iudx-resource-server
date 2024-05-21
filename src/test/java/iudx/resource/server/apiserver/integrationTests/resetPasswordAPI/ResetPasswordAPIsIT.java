93488feb5b8f60e8b3c9172c29eb88d89f91889e
Ankit Singh <101859999+ankitmashu@users.noreply.github.com>
2024-05-21 13:44:36 +0530

    response refractor

--- src/test/java/iudx/resource/server/apiserver/integrationTests/resetPasswordAPI/ResetPasswordAPIsIT.java
+++ src/test/java/iudx/resource/server/apiserver/integrationTests/resetPasswordAPI/ResetPasswordAPIsIT.java
@@ -32,7 +32,8 @@ public class ResetPasswordAPIsIT {
                 .post("/user/resetPassword")
                 .then()
                 .statusCode(200)
-                .body("title", equalTo("Successfully changed the password"))
+                .body("title", equalTo("success"))
+                .body("detail", equalTo("Successfully changed the password"))
                 .extract()
                 .response();
     }
