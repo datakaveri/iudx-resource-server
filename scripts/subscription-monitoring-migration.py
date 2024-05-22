d8da7de39c9953ff6307446d76fa887991fcb241
SRINI <106464244+SRINI2410@users.noreply.github.com>
2024-05-22 15:21:46 +0530

    Update subscription-monitoring-migration script
    Commented out queue_declare section

--- scripts/subscription-monitoring-migration.py
+++ scripts/subscription-monitoring-migration.py
@@ -52,7 +52,7 @@ for exchange in rows:
     connection = pika.BlockingConnection(pika.URLParameters(f'amqps://{dataBrokerUserName}:{encoded_pass}@{dataBrokerHost}:{dataBrokerPort}/{vhost}'))
     channel= connection.channel()
     channel.exchange_declare(dataBrokerExchange, durable=True, exchange_type="topic")
-    channel.queue_declare(dataBrokerQueue)
+    # channel.queue_declare(dataBrokerQueue)
     channel.queue_bind(exchange=dataBrokerExchange, queue=dataBrokerQueue, routing_key=f"{dataBrokerExchange}/.*")
     print(f'"{dataBrokerExchange}" is bound with queue "{dataBrokerQueue}"\n')
     connection.close()
